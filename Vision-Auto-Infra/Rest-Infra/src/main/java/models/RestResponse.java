158b104006768813c99eb94cced7794026dd1b94
mohamadi <mohamadi@radware.com>
2019-12-17 18:46:45 +0200

    Add RestResponse Model

--- Vision-Auto-Infra/Rest-Infra/src/main/java/models/RestResponse.java
+++ Vision-Auto-Infra/Rest-Infra/src/main/java/models/RestResponse.java
@@ -1,4 +1,23 @@
 package models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.apache.http.entity.ContentType;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
 public class RestResponse {
+
+    private StatusCode statusCode;
+    private String statusLine;
+    private Body body;
+    private Map<String, String> headers;
+    private Map<String, String> cookies;
+    private ContentType contentType;
+    private String sessionId;
+    private long time;
+    private List<String> errors;
 }
