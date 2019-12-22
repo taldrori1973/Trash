3062dc32ac00d248c40db85fef22fdeb6ea4ac76
mohamadi <mohamadi@radware.com>
2019-12-22 13:01:41 +0200

    add utils package

--- Vision-Auto-Infra/Rest-Infra/src/main/java/models/RestRequestSpecification.java
+++ Vision-Auto-Infra/Rest-Infra/src/main/java/models/RestRequestSpecification.java
@@ -3,7 +3,7 @@ package models;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
-import mappers.MapUtils;
+import utils.MapUtils;
 
 import java.util.HashMap;
 import java.util.Map;
--- Vision-Auto-Infra/Rest-Infra/src/main/java/utils/MapUtils.java
+++ Vision-Auto-Infra/Rest-Infra/src/main/java/utils/MapUtils.java
@@ -1,4 +1,4 @@
-package mappers;
+package utils;
 
 import java.util.Map;
 
