7fdbf88cf8fa787d7f5a92688c14f113e10734be
mohamadi <radware>
2020-03-30 14:11:03 +0300

    VisionConfiguration

---
+++ Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/systemManagement/models/VisionConfigurations.java
@@ -0,0 +1,14 @@
+package com.radware.vision.systemManagement.models;
+
+public class VisionConfigurations {
+
+    private String macAddress;
+    private String activeServerMacAddress;
+    private String hostname;
+    private String defenseFlowId;
+    private String hardwarePlatform;
+    private String version;
+    private String build;
+
+
+}
