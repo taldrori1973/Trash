c3c7fd7bffeadf6ebbb41266a8f0c2b128213bec
mohamadi <radware>
2020-03-31 15:12:13 +0300

    VisionConfigurationsModel

--- Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/systemManagement/controllers/VisionConfigurationsController.java
+++ Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/systemManagement/controllers/VisionConfigurationsController.java
@@ -1,4 +1,40 @@
 package com.radware.vision.systemManagement.controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.radware.vision.systemManagement.models.VisionConfigurationsModel;
+import com.radware.vision.tools.rest.CurrentVisionRestAPI;
+import models.RestResponse;
+import models.StatusCode;
+
 public class VisionConfigurationsController {
+
+    public VisionConfigurationsModel getVisionConfigurationsByRest() {
+        VisionConfigurationsModel visionConfigurationsModel = new VisionConfigurationsModel();
+        try {
+            CurrentVisionRestAPI currentVisionRestAPI = new CurrentVisionRestAPI("Vision/SystemManagement.json", "Get Management Info");
+
+            RestResponse result = currentVisionRestAPI.sendRequest();
+
+            if (!result.getStatusCode().equals(StatusCode.OK))
+                throw new UnsupportedOperationException(result.getBody().getBodyAsString());
+
+            JsonNode body = result.getBody().getBodyAsJsonNode().isPresent() ? result.getBody().getBodyAsJsonNode().get() : null;
+            if (body == null) throw new NullPointerException("Request Body returns null");
+
+            visionConfigurationsModel.setActiveServerMacAddress(body.get("activeServerMacAddress").asText());
+            visionConfigurationsModel.setDefenseFlowId(body.get("defenseFlowId").asText());
+            visionConfigurationsModel.setHardwarePlatform(body.get("hardwarePlatform").asText());
+            visionConfigurationsModel.setHostname(body.get("hostname").asText());
+            visionConfigurationsModel.setMacAddress(body.get("macAddress").asText());
+
+            String[] versionAndBuild = body.get("activeServerMacAddress").asText().split(" ");
+
+
+        } catch (NoSuchFieldException | UnsupportedOperationException e) {
+            e.printStackTrace();
+        }
+
+
+        return null;
+    }
 }
---
+++ Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/systemManagement/models/VisionConfigurationsModel.java
@@ -0,0 +1,19 @@
+package com.radware.vision.systemManagement.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class VisionConfigurationsModel {
+
+    private String macAddress;
+    private String activeServerMacAddress;
+    private String hostname;
+    private String defenseFlowId;
+    private String hardwarePlatform;
+    private String version;
+    private String build;
+
+
+}
