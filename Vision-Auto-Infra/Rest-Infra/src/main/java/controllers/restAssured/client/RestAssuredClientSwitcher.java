441999c3022077f7b5c793d646d061bf9e609583
mohamadi <radware>
2020-04-08 19:07:05 +0300

    Clear

--- Vision-Auto-Infra/Rest-Infra/src/main/java/controllers/restAssured/client/RestAssuredClientSwitcher.java
+++ Vision-Auto-Infra/Rest-Infra/src/main/java/controllers/restAssured/client/RestAssuredClientSwitcher.java
@@ -1,21 +1,12 @@
 package controllers.restAssured.client;
 
 import controllers.RestClientsManagement;
-import controllers.restAssured.client.BasicAuth.RestAssuredBasicAuthBasedRestClient;
-import controllers.restAssured.client.SessionBased.RestAssuredSessionBasedRestClient;
 import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
 import restInterface.client.RestClient;
 
 public class RestAssuredClientSwitcher {
-    public static void switchTo(RestClient restClient, Class<RestClient> restClientClass) {
 
-        if (restClientClass.equals(RestAssuredSessionBasedRestClient.class)) {
-
-        } else if (restClientClass.equals(RestAssuredBasicAuthBasedRestClient.class)) {
-
-        }
-    }
 
     public static void switchTo(String baseUri, int connectionPort, String sessionId, RequestSpecification requestSpecification, RestClient restClient) {
         RestAssured.baseURI = baseUri;
