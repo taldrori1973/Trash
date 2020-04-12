4f4a09ac8a902f93368e638866bc9826188c9082
mohamadi <radware>
2020-04-12 16:35:18 +0300

    DevicesRepository

---
+++ Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/DeviceType.java
@@ -0,0 +1,20 @@
+package com.radware.vision.automation.AutoUtils.SUT;
+
+public enum DeviceType {
+
+    ALTEON("Alteon"),
+    LINK_PROOF("LinkProof"),
+    DEFENSE_PRO("DefensePro"),
+    APPWALL("AppWall");
+
+
+    private String type;
+
+    DeviceType(String type) {
+
+    }
+
+    public String getType() {
+        return type;
+    }
+}
--- Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/repositories/DevicesRepository.java
+++ Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/repositories/DevicesRepository.java
@@ -1,6 +1,18 @@
 package com.radware.vision.automation.AutoUtils.SUT.repositories;
 
+import com.radware.vision.automation.AutoUtils.SUT.DeviceType;
+import com.radware.vision.automation.AutoUtils.SUT.repositories.pojos.devices.Devices;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DevicesRepository {
 
+    private Devices devices;
+
+    public List<Devices> findDevicesByType(DeviceType deviceType) {
+
+        return new ArrayList<>();
+    }
 
 }
