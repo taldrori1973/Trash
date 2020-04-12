21e2c31a261996de26ca89e7af65a8286694bd7c
mohamadi <radware>
2020-04-12 16:14:17 +0300

    setups

--- Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/repositories/pojos/setup/Device.java
+++ Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/repositories/pojos/setup/Device.java
@@ -1,4 +1,9 @@
 package com.radware.vision.automation.AutoUtils.SUT.repositories.pojos.setup;
 
+import lombok.Data;
+
+@Data
 public class Device {
+    private String parentSite;
+    private String deviceId;
 }
--- Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/repositories/pojos/setup/Site.java
+++ Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/repositories/pojos/setup/Site.java
@@ -1,4 +1,10 @@
 package com.radware.vision.automation.AutoUtils.SUT.repositories.pojos.setup;
 
+import lombok.Data;
+
+@Data
 public class Site {
+
+    private String parent;
+    private String name;
 }
