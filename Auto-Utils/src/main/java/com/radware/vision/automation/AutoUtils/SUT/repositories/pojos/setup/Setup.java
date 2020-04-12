9bd4d6af6a5447d21853fd49188975d924b3a1a7
mohamadi <radware>
2020-04-12 16:12:35 +0300

    setups

--- Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/repositories/pojos/setup/Setup.java
+++ Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/repositories/pojos/setup/Setup.java
@@ -1,4 +1,12 @@
 package com.radware.vision.automation.AutoUtils.SUT.repositories.pojos.setup;
 
+
+import java.util.List;
+
 public class Setup {
+
+    private String setupId;
+    private List<Site> sites;
+    private List<Device> devices;
+
 }
---
+++ Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/repositories/pojos/setup/Site.java
@@ -0,0 +1,4 @@
+package com.radware.vision.automation.AutoUtils.SUT.repositories.pojos.setup;
+
+public class Site {
+}
