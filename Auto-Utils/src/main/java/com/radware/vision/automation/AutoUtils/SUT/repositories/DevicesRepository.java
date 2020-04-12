57f6f42ffc7b1e7d68a82ed727fd9ff70e0be2fd
mohamadi <radware>
2020-04-12 16:48:28 +0300

    find devices by type

--- Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/repositories/DevicesRepository.java
+++ Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/repositories/DevicesRepository.java
@@ -1,18 +1,30 @@
 package com.radware.vision.automation.AutoUtils.SUT.repositories;
 
 import com.radware.vision.automation.AutoUtils.SUT.DeviceType;
+import com.radware.vision.automation.AutoUtils.SUT.repositories.pojos.devices.Device;
 import com.radware.vision.automation.AutoUtils.SUT.repositories.pojos.devices.Devices;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class DevicesRepository {
 
     private Devices devices;
 
-    public List<Devices> findDevicesByType(DeviceType deviceType) {
-
-        return new ArrayList<>();
+    public List<Device> findDevicesByType(DeviceType deviceType) {
+        switch (deviceType) {
+            case ALTEON:
+                return devices.getTreeDevices().getAlteons();
+            case LINK_PROOF:
+                return devices.getTreeDevices().getLinkProofs()
+            case DEFENSE_PRO:
+                return devices.getTreeDevices().getDefensePros();
+            case APPWALL:
+                return devices.getTreeDevices().getAppWalls();
+            default:
+                return Collections.EMPTY_LIST;
+        }
     }
+
 
 }
