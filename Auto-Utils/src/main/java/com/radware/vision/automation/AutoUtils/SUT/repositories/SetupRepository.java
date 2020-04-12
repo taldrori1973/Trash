2c5c191a567d2d3054b67c9c8c2546835926aacc
mohamadi <radware>
2020-04-12 17:07:27 +0300

    Setup Repo

---
+++ Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/repositories/SetupRepository.java
@@ -0,0 +1,30 @@
+package com.radware.vision.automation.AutoUtils.SUT.repositories;
+
+import com.radware.vision.automation.AutoUtils.SUT.repositories.pojos.setup.Device;
+import com.radware.vision.automation.AutoUtils.SUT.repositories.pojos.setup.Setup;
+import com.radware.vision.automation.AutoUtils.SUT.repositories.pojos.setup.Site;
+
+import java.util.List;
+import java.util.Optional;
+
+public class SetupRepository {
+
+    private Setup setup;
+
+    public Optional<Site> findSiteByName(String siteName) {
+        return setup.getSites().stream().filter(site -> site.getName().equals(siteName)).findAny();
+    }
+
+    public Optional<Device> findDeviceById(String deviceId) {
+        return setup.getDevices().stream().filter(device -> device.getDeviceId().equals(deviceId)).findAny();
+    }
+
+    public List<Device> findAllDevices() {
+        return setup.getDevices();
+    }
+
+    public List<Site> findAllSites() {
+        return setup.getSites();
+    }
+
+}
