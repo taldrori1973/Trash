ae5ec94e13882a864ada4037c7a59a5abdaa80d1
mohamadi <radware>
2020-04-13 15:26:40 +0300

    SUT Pojo

---
+++ Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/repositories/pojos/sut/SUT.java
@@ -0,0 +1,10 @@
+package com.radware.vision.automation.AutoUtils.SUT.repositories.pojos.sut;
+
+import lombok.Data;
+
+@Data
+public class SUT {
+    private String setup;
+    private VisionConfiguration visionConfiguration;
+
+}
---
+++ Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/repositories/pojos/sut/VisionConfiguration.java
@@ -0,0 +1,12 @@
+package com.radware.vision.automation.AutoUtils.SUT.repositories.pojos.sut;
+
+import lombok.Data;
+
+@Data
+public class VisionConfiguration {
+
+    private String hostIp;
+    private String userName;
+    private String password;
+
+}
