549b65bd490f9daea6213a9c284211c813cab600
mohamadi <radware>
2020-04-13 16:14:58 +0300

    SUT signleton

--- Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/SUT.java
+++ Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/SUT.java
@@ -2,13 +2,19 @@ package com.radware.vision.automation.AutoUtils.SUT;
 
 import lombok.Data;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+import java.util.List;
+
 @Data
 public class SUT {
 
-
+    private static RuntimeMXBean runtimeMXBean = ManagementFactory.getRuntimeMXBean();
     private static final SUT instance = new SUT();
 
+
     private SUT() {
+        List<String> arguments = runtimeMXBean.getInputArguments();
     }
 
     public static SUT getInstance() {
