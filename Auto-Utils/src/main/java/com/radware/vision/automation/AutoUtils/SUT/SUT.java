18fe0f60959e2ff71121eabb4935037b5991a25c
mohamadi <radware>
2020-04-13 16:36:14 +0300

    Properties

--- Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/SUT.java
+++ Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/SUT.java
@@ -2,12 +2,15 @@ package com.radware.vision.automation.AutoUtils.SUT;
 
 import lombok.Data;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+import java.util.regex.Pattern;
 
 @Data
 public class SUT {
@@ -16,19 +19,31 @@ public class SUT {
     private static final SUT instance = new SUT();
 
 
-    private SUT() throws FileNotFoundException {
-        Properties properties = loadApplicationProperties();
+    private SUT() {
 
-        properties.
-                List<String> vmOptions = runtimeMXBean.getInputArguments();
+        Properties properties = loadApplicationProperties();//load environment/application.properties file from resources
+
+        String sutArgument = properties.getProperty("SUT.vmOptions.key");//returns the "-DSUT" as the key of the sut in vm options
+
+        Map<String, String> sutPaths = getSutPaths(sutArgument);
+
+
+        List<String> vmOptions = runtimeMXBean.getInputArguments();
     }
 
-    private Properties loadApplicationProperties() throws FileNotFoundException {
+    private Map<String, String> getSutPaths(String sutArgument) {
+        Map<String, String> pathsMap = new HashMap<>();
+        Pattern pattern = Pattern.compile("([^=]*)=([^=]*)");
+        return pathsMap;
+    }
+
+    private Properties loadApplicationProperties() {
         Properties properties = new Properties();
         try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("environment/application.properties")) {
             properties.load(inputStream);
         } catch (IOException e) {
-            throw new FileNotFoundException(e.getMessage());
+            System.err.println(e.getMessage());
+            e.printStackTrace();
         }
         return properties;
     }
