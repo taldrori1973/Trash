e609e6762474c93a2b94524917dbb574bc53c41c
mohamadi <radware>
2020-04-13 16:06:36 +0300

    SUT signleton

--- Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/SUT.java
+++ Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/SUT.java
@@ -1,11 +1,17 @@
 package com.radware.vision.automation.AutoUtils.SUT;
 
-import com.radware.vision.automation.AutoUtils.SUT.repositories.pojos.sut.VisionConfiguration;
 import lombok.Data;
 
 @Data
 public class SUT {
-    private String setup;
-    private VisionConfiguration visionConfiguration;
 
+
+    private static final SUT instance = new SUT();
+
+    private SUT() {
+    }
+
+    public static public static SUT getInstance() {
+        return instance;
+    }
 }
