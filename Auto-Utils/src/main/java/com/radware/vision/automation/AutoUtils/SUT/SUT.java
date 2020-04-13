3c396eb5fd67bdb824d3edd328dac4043a401cf6
mohamadi <radware>
2020-04-13 16:24:13 +0300

    Properties

--- Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/SUT.java
+++ Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/SUT.java
@@ -2,9 +2,12 @@ package com.radware.vision.automation.AutoUtils.SUT;
 
 import lombok.Data;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
-import java.util.List;
+import java.util.Properties;
 
 @Data
 public class SUT {
@@ -13,8 +16,21 @@ public class SUT {
     private static final SUT instance = new SUT();
 
 
-    private SUT() {
-        List<String> arguments = runtimeMXBean.getInputArguments();
+    private SUT() throws FileNotFoundException {
+        Properties properties = loadApplicationProperties();
+
+        properties.
+                List<String> vmOptions = runtimeMXBean.getInputArguments();
+    }
+
+    private Properties loadApplicationProperties() throws FileNotFoundException {
+        Properties properties = new Properties();
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("environment/application.properties")) {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            throw new FileNotFoundException(e.getMessage());
+        }
+        return properties;
     }
 
     public static SUT getInstance() {
