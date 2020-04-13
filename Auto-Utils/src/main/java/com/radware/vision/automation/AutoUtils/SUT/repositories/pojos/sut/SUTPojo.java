531e2e2e2397e9187da39c9b3448ada4f69537ef
mohamadi <radware>
2020-04-13 15:31:05 +0300

    SUT Pojo

--- Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/repositories/pojos/sut/SUTPojo.java
+++ Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/repositories/pojos/sut/SUTPojo.java
@@ -3,7 +3,7 @@ package com.radware.vision.automation.AutoUtils.SUT.repositories.pojos.sut;
 import lombok.Data;
 
 @Data
-public class SUT {
+public class SUTPojo {
     private String setup;
     private VisionConfiguration visionConfiguration;
 
