1c28867573a27d9053882d6f9db6fd15888f64d3
mohamadi <radware>
2020-04-15 15:53:34 +0300

    fix

--- Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/controllers/SUTManagerImpl.java
+++ Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/controllers/SUTManagerImpl.java
@@ -33,7 +33,7 @@ public class SUTManagerImpl implements SUTManager {
     private static final String SUT_FILES_PATH_PROPERTY = "SUT.path";
     private static final String SUT_SETUPS_FILES_PATH_PROPERTY = "SUT.setups.path";
     private static final String SUT_DEVICES_FILES_PATH_PROPERTY = "SUT.devices.path";
-    private static final String DEVICES_FILE_NAME = "devices";
+    private static final String DEVICES_FILE_NAME = "devices.json";
 
 
     private ApplicationPropertiesUtils applicationPropertiesUtils;
