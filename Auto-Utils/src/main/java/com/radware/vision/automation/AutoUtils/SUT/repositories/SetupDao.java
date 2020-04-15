6b5e1820cd7041e9933bec709809bbaa428c967c
mohamadi <radware>
2020-04-15 14:03:54 +0300

    Add Rest to SUT

--- Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/repositories/SetupDao.java
+++ Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/repositories/SetupDao.java
@@ -7,32 +7,32 @@ import com.radware.vision.automation.AutoUtils.SUT.repositories.pojos.setup.Site
 import java.util.List;
 import java.util.Optional;
 
-public class SetupRepository {
+public class SetupDao {
 
     private Setup setup;
 
-    public SetupRepository(Setup setup) {
+    public SetupDao(Setup setup) {
         this.setup = setup;
     }
 
     public Optional<Site> findSiteByName(String siteName) {
-        return setup.getSites().stream().filter(site -> site.getName().equals(siteName)).findAny();
+        return setup.getTree().getSites().stream().filter(site -> site.getName().equals(siteName)).findAny();
     }
 
     public Optional<Device> findDeviceById(String deviceId) {
-        return setup.getDevices().stream().filter(device -> device.getDeviceId().equals(deviceId)).findAny();
+        return setup.getTree().getDevices().stream().filter(device -> device.getDeviceId().equals(deviceId)).findAny();
     }
 
     public List<Device> findAllDevices() {
-        return setup.getDevices();
+        return setup.getTree().getDevices();
     }
 
     public List<Site> findAllSites() {
-        return setup.getSites();
+        return setup.getTree().getSites();
     }
 
     public boolean isDeviceExistById(String deviceId) {
-        Optional<Device> filtered = this.setup.getDevices().stream().filter(device -> device.getDeviceId().equals(deviceId)).findFirst();
+        Optional<Device> filtered = this.setup.getTree().getDevices().stream().filter(device -> device.getDeviceId().equals(deviceId)).findFirst();
         return filtered.isPresent();
     }
 }
