c216af2108f65faf209703f09832e5cb3020dffe
mohamadi <radware>
2020-04-15 13:47:39 +0300

    Change template

--- Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/repositories/pojos/setup/Setup.java
+++ Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/repositories/pojos/setup/Setup.java
@@ -3,14 +3,12 @@ package com.radware.vision.automation.AutoUtils.SUT.repositories.pojos.setup;
 
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 public class Setup {
 
     private String setupId;
     private Tree tree;
-    private List<Site> sites;
-    private List<Device> devices;
+    private String defenseFlowId;
+
 
 }
