46557039e0eb78a3f9da7558be52edad2f23e69e
mohamadi <radware>
2020-04-16 21:32:57 +0300

    Add SUT Service

--- Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/services/SutService.java
+++ Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/services/SutService.java
@@ -1,11 +1,19 @@
 package com.radware.vision.automation.AutoUtils.SUT.services;
 
+import com.radware.vision.automation.AutoUtils.SUT.repositories.DevicesDao;
+import com.radware.vision.automation.AutoUtils.SUT.repositories.SetupDao;
 import com.radware.vision.automation.AutoUtils.SUT.repositories.pojos.devices.Devices;
 import com.radware.vision.automation.AutoUtils.SUT.repositories.pojos.setup.Setup;
 import com.radware.vision.automation.AutoUtils.SUT.repositories.pojos.sut.SUTPojo;
 
-public class SutService {
-    public SutService(Devices devicesPojo, SUTPojo sutPojo, Setup setupPojo) {
 
+public class SutService {
+
+    private DevicesDao devicesDao;
+    private SetupDao setupDao;
+
+    public SutService(Devices devicesPojo, SUTPojo sutPojo, Setup setupPojo) {
+        this.devicesDao = new DevicesDao(devicesPojo);
+        this.setupDao = new SetupDao(setupPojo);
     }
 }
