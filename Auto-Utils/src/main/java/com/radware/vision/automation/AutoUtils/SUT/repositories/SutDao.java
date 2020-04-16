8b3e7014be51b0db762f363532ac37731b17bd6e
mohamadi <radware>
2020-04-16 21:37:08 +0300

    Init Service

---
+++ Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/repositories/SutDao.java
@@ -0,0 +1,13 @@
+package com.radware.vision.automation.AutoUtils.SUT.repositories;
+
+import com.radware.vision.automation.AutoUtils.SUT.repositories.pojos.sut.SUTPojo;
+
+public class SutDao {
+
+    private SUTPojo sutPojo;
+
+    public SutDao(SUTPojo sutPojo) {
+        this.sutPojo = sutPojo;
+    }
+
+}
--- Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/services/SutService.java
+++ Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/services/SutService.java
@@ -2,6 +2,7 @@ package com.radware.vision.automation.AutoUtils.SUT.services;
 
 import com.radware.vision.automation.AutoUtils.SUT.repositories.DevicesDao;
 import com.radware.vision.automation.AutoUtils.SUT.repositories.SetupDao;
+import com.radware.vision.automation.AutoUtils.SUT.repositories.SutDao;
 import com.radware.vision.automation.AutoUtils.SUT.repositories.pojos.devices.Devices;
 import com.radware.vision.automation.AutoUtils.SUT.repositories.pojos.setup.Setup;
 import com.radware.vision.automation.AutoUtils.SUT.repositories.pojos.sut.SUTPojo;
@@ -11,9 +12,11 @@ public class SutService {
 
     private DevicesDao devicesDao;
     private SetupDao setupDao;
+    private SutDao sutDao;
 
     public SutService(Devices devicesPojo, SUTPojo sutPojo, Setup setupPojo) {
         this.devicesDao = new DevicesDao(devicesPojo);
         this.setupDao = new SetupDao(setupPojo);
+        this.sutDao = new SutDao(sutPojo);
     }
 }
