251108ae326790813b8382103293a5ecefb68fd3
mohamadi <radware>
2020-04-19 14:20:55 +0300

    Change Names

--- Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/dtos/VisionConfigurationDto.java
+++ Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/dtos/VisionConfigurationDto.java
@@ -1,4 +1,11 @@
 package com.radware.vision.automation.AutoUtils.SUT.dtos;
 
 public class VisionConfigurationDto {
+
+    private String hostIp;
+    private String userName;
+    private String password;
+    private String restConnectionDefaultPort;
+    private String restConnectionDefaultProtocol;
+
 }
