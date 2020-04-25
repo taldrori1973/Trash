bb507505b1ae1f3251828bb133843cf94b6acd00
mohamadi <radware>
2020-04-26 00:47:49 +0300

    Delete Jsystem Scenarios

--- Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/tests/rbac/DefensePro/monitoring/operationalStatus/RBACDefenseProResourceUtilizationTableActionTests.java
+++
@@ -1,9 +0,0 @@
-package com.radware.vision.tests.rbac.DefensePro.monitoring.operationalStatus;
-
-import com.radware.vision.tests.rbac.RBACTestBase;
-
-/**
- * Created by stanislava on 10/2/2014.
- */
-public class RBACDefenseProResourceUtilizationTableActionTests extends RBACTestBase {
-}
