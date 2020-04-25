1276d5fad34e023021dfe32d4eefadeeaa5787e6
mohamadi <radware>
2020-04-25 22:16:05 +0300

    Fixing

--- Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/tests/GeneralOperations/GeneralOperationsTests.java
+++ Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/tests/GeneralOperations/GeneralOperationsTests.java
@@ -1,5 +1,6 @@
 package com.radware.vision.tests.GeneralOperations;
 
+import com.radware.automation.tools.basetest.BaseTestUtils;
 import com.radware.automation.tools.basetest.Reporter;
 import com.radware.automation.tools.utils.FileUtils;
 import com.radware.automation.tools.utils.StringUtils;
--- Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/tests/rbac/configuration/applicationdelivery/RBACAlteonGlobalTrafficRedirectionTableActionTests.java
+++ Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/tests/rbac/configuration/applicationdelivery/RBACAlteonGlobalTrafficRedirectionTableActionTests.java
@@ -1,5 +1,6 @@
 package com.radware.vision.tests.rbac.configuration.applicationdelivery;
 
+import com.radware.automation.tools.basetest.BaseTestUtils;
 import com.radware.automation.tools.basetest.Reporter;
 import com.radware.vision.infra.testhandlers.rbac.configuration.applicationdelivery.RBACAlteonGlobalTrafficRedirectionTableActionHandler;
 import com.radware.vision.infra.testhandlers.rbac.enums.AddDeleteTableActions;
