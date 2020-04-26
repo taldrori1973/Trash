47666397439db176c7c208c11f13d264261801a6
mohamadi <radware>
2020-04-26 23:46:15 +0300

    New Cli Module

---
+++ Vision-Auto-Infra/CLI-Infra/src/main/java/com/radware/vision/automation/VisionAutoInfra/CLIInfra/Servers/LinuxFileServer.java
@@ -0,0 +1,102 @@
+package com.radware.vision.automation.VisionAutoInfra.CLIInfra.Servers;
+
+import systemobject.terminal.Prompt;
+
+import java.util.ArrayList;
+
+public class LinuxFileServer extends ServerCliBase {
+
+
+    public LinuxFileServer() {
+    }
+
+    public LinuxFileServer(String host, String user, String password) {
+        super(host, user, password);
+    }
+
+    @Override
+    public void init() throws Exception {
+//		report.startLevel("Initing LinuxFileServer");
+        super.init();
+//		report.stopLevel();
+    }
+
+    public void close() {
+        super.close();
+    }
+
+
+    @Override
+    public Prompt[] getPrompts() {
+        ArrayList<Prompt> prompts = new ArrayList<Prompt>();
+        Prompt p = new Prompt();
+        p.setCommandEnd(true);
+        p.setPrompt("# ");
+        prompts.add(p);
+
+        p = new Prompt();
+        p.setCommandEnd(true);
+        p.setPrompt("$ ");
+        prompts.add(p);
+
+        p = new Prompt();
+        p.setPrompt("login as: ");
+        p.setStringToSend(getUser());
+        prompts.add(p);
+
+        p = new Prompt();
+        p.setPrompt("User:.*");
+        p.setRegularExpression(true);
+        p.setStringToSend(getUser());
+        prompts.add(p);
+
+        p = new Prompt();
+        p.setPrompt("Password:.*");
+        p.setRegularExpression(true);
+        p.setCommandEnd(true);
+        prompts.add(p);
+
+        p = new Prompt();
+        p.setPrompt("s' password: ");
+        p.setCommandEnd(true);
+        prompts.add(p);
+
+        p = new Prompt();
+        p.setPrompt("password: ");
+        p.setCommandEnd(true);
+        prompts.add(p);
+
+        p = new Prompt();
+        p.setPrompt("mysql>");
+        p.setCommandEnd(true);
+        prompts.add(p);
+
+        p = new Prompt();
+        p.setPrompt("(yes/no)?");
+        p.setCommandEnd(true);
+        prompts.add(p);
+
+
+        p = new Prompt();
+        p.setPrompt("(yes/no)? ");
+        p.setCommandEnd(true);
+        prompts.add(p);
+
+        // DefensePro Login - new prompt added
+        p = new Prompt();
+        p.setPrompt("DefensePro#");
+        p.setCommandEnd(true);
+        prompts.add(p);
+
+        // DefensePro Reboot - new prompt added
+        p = new Prompt();
+        p.setPrompt("(y/n)[n]:");
+        p.setCommandEnd(true);
+        prompts.add(p);
+
+
+        return prompts.toArray(new Prompt[prompts.size()]);
+    }
+
+
+}
---
+++ Vision-Auto-Infra/CLI-Infra/src/main/java/com/radware/vision/automation/VisionAutoInfra/CLIInfra/Servers/ServerCliBase.java
@@ -0,0 +1,52 @@
+package com.radware.vision.automation.VisionAutoInfra.CLIInfra.Servers;
+
+import com.aqua.sysobj.conn.LinuxDefaultCliConnection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by urig on 6/14/2017.
+ */
+public class ServerCliBase extends LinuxDefaultCliConnection {
+
+
+    public ServerCliBase() {
+        super();
+    }
+
+    public ServerCliBase(String host, String user, String password) {
+        super(host, user, password);
+    }
+
+    /**
+     * This function returns the output of the cmd without the first line ( the cmd you send )
+     * and without the last line ( the new prompt )
+     * handels CliConnection.getTestAgainstObject().toString()F
+     *
+     * @author izikp
+     */
+    public ArrayList<String> getCmdOutput() {
+
+        ArrayList<String> resultLines = new ArrayList<String>();
+        if (getTestAgainstObject() != null) {
+            String[] lines = getTestAgainstObject().toString().split("[\\r\\n]+");
+            for (int i = 1; i < lines.length - 1; i++) {
+                resultLines.add(lines[i]);
+            }
+        }
+        return resultLines;
+    }
+
+
+    /**
+     * @return the last row of the result
+     */
+    public String getLastRow() {
+
+        List<String> results = getCmdOutput();
+        if (results.size() > 0)
+            return results.get(results.size() - 1);
+        return null;
+    }
+}
