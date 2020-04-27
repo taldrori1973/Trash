98377b4042a1839b5654d0d8c0953512cec2b4ef
mohamadi <radware>
2020-04-27 23:45:10 +0300

    For now no need for root/radware server cli

--- Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/systemManagement/ServersManagement.java
+++ Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/systemManagement/ServersManagement.java
@@ -2,8 +2,6 @@ package com.radware.vision.systemManagement;
 
 import com.radware.vision.automation.AutoUtils.SUT.dtos.ServerDto;
 import com.radware.vision.automation.VisionAutoInfra.CLIInfra.Servers.LinuxFileServer;
-import com.radware.vision.automation.VisionAutoInfra.CLIInfra.Servers.RadwareServerCli;
-import com.radware.vision.automation.VisionAutoInfra.CLIInfra.Servers.RootServerCli;
 import com.radware.vision.automation.VisionAutoInfra.CLIInfra.Servers.ServerCliBase;
 import com.radware.vision.base.TestBase;
 
@@ -14,13 +12,13 @@ public class ServersManagement {
 
 
     private LinuxFileServer linuxFileServer;
-    private RadwareServerCli radwareServerCli;
-    private RootServerCli rootServerCli;
+//    private RadwareServerCli radwareServerCli;
+//    private RootServerCli rootServerCli;
 
     public ServersManagement() {
         this.linuxFileServer = this.createAndInitServer(ServerIds.LINUX_FILE_SERVER, LinuxFileServer.class);
-        this.radwareServerCli = this.createAndInitServer(ServerIds.RADWARE_SERVER_CLI, RadwareServerCli.class);
-        this.rootServerCli = this.createAndInitServer(ServerIds.ROOT_SERVER_CLI, RootServerCli.class);
+//        this.radwareServerCli = this.createAndInitServer(ServerIds.RADWARE_SERVER_CLI, RadwareServerCli.class);
+//        this.rootServerCli = this.createAndInitServer(ServerIds.ROOT_SERVER_CLI, RootServerCli.class);
     }
 
     private <SERVER extends ServerCliBase> SERVER createAndInitServer(ServerIds serverId, Class<SERVER> clazz) {
@@ -43,18 +41,18 @@ public class ServersManagement {
         return this.linuxFileServer != null ? Optional.of(linuxFileServer) : Optional.empty();
     }
 
-    public Optional<RadwareServerCli> getRadwareServerCli() {
-        return this.radwareServerCli != null ? Optional.of(radwareServerCli) : Optional.empty();
-    }
-
-    public Optional<RootServerCli> getRootServerCLI() {
-        return this.rootServerCli != null ? Optional.of(rootServerCli) : Optional.empty();
-    }
+//    public Optional<RadwareServerCli> getRadwareServerCli() {
+//        return this.radwareServerCli != null ? Optional.of(radwareServerCli) : Optional.empty();
+//    }
+//
+//    public Optional<RootServerCli> getRootServerCLI() {
+//        return this.rootServerCli != null ? Optional.of(rootServerCli) : Optional.empty();
+//    }
 
     enum ServerIds {
-        LINUX_FILE_SERVER("linuxFileServer"),
-        RADWARE_SERVER_CLI("radwareServerCli"),
-        ROOT_SERVER_CLI("rootServerCli");
+        LINUX_FILE_SERVER("linuxFileServer");
+//        RADWARE_SERVER_CLI("radwareServerCli"),
+//        ROOT_SERVER_CLI("rootServerCli");
 
         private String serverId;
 
