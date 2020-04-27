86b9df858bd138e04c579f2a7c575592903aeea3
mohamadi <radware>
2020-04-28 00:19:52 +0300

    TODO FilesHandler

--- Vision-Auto-Tests/Webui-Step-Handlers/src/main/java/com/radware/vision/infra/testhandlers/cli/FilesHandler.java
+++ Vision-Auto-Tests/Webui-Step-Handlers/src/main/java/com/radware/vision/infra/testhandlers/cli/FilesHandler.java
@@ -1,7 +1,6 @@
 package com.radware.vision.infra.testhandlers.cli;
 
 import com.radware.automation.tools.cli.ServerCliBase;
-import com.radware.vision.automation.VisionAutoInfra.CLIInfra.CliOperations;
 
 /**
  * Created by MoaadA on 1/6/2019.
@@ -9,12 +8,12 @@ import com.radware.vision.automation.VisionAutoInfra.CLIInfra.CliOperations;
 public class FilesHandler {
 
     public void scp(String srcPath, ServerCliBase srcMachine, ServerCliBase destMachine, String destPath) {
-
-        CliOperations.runCommand(srcMachine, "scp -r " + srcPath + " " + destMachine.getUser() + "@" + destMachine.getHost() + ":" + destPath, 5 * 1000);
-        if (CliOperations.lastOutput.contains("Are you sure you want to continue connecting (yes/no)?"))
-            CliOperations.runCommand(srcMachine, "yes", 5 * 1000);
-
-        if (CliOperations.lastOutput.contains("password"))
-            CliOperations.runCommand(srcMachine, destMachine.getPassword(), 10 * 1000);
+//          kVision srcMachine and destMachine
+//        CliOperations.runCommand(srcMachine, "scp -r " + srcPath + " " + destMachine.getUser() + "@" + destMachine.getHost() + ":" + destPath, 5 * 1000);
+//        if (CliOperations.lastOutput.contains("Are you sure you want to continue connecting (yes/no)?"))
+//            CliOperations.runCommand(srcMachine, "yes", 5 * 1000);
+//
+//        if (CliOperations.lastOutput.contains("password"))
+//            CliOperations.runCommand(srcMachine, destMachine.getPassword(), 10 * 1000);
     }
 }
