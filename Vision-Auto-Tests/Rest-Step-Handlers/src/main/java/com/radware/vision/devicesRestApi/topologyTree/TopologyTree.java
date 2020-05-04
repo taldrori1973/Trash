dac642528b74a89ee81d0920c264205e3dda9288
mohamadi <radware>
2020-05-05 01:28:22 +0300

    TopologyTree

--- Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/devicesRestApi/topologyTree/TopologyTree.java
+++ Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/devicesRestApi/topologyTree/TopologyTree.java
@@ -1,9 +1,28 @@
 package com.radware.vision.devicesRestApi.topologyTree;
 
+import com.radware.vision.RestStepResult;
+
 /**
  * Created by MohamadI - Muhamad Igbaria
  * Date: 5/5/2020
  * Time: 1:24 AM
  */
 public interface TopologyTree {
+
+    RestStepResult addDevice(String setId);
+
+    RestStepResult getDevice(String setId);
+
+    RestStepResult updateDevice(String setId);
+
+    RestStepResult deleteDevice(String setId);
+
+    RestStepResult addSite(String siteName, String parentSiteName);
+
+    RestStepResult getSite(String siteName);
+
+
+    RestStepResult deleteSite(String siteName);
+
+
 }
