0d5e6e34a3f340e37bd080fb9de94c33fc5b4850
mohamadi <radware>
2020-05-05 01:33:46 +0300

    impl

---
+++ Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/devicesRestApi/topologyTree/TopologyTreeImpl.java
@@ -0,0 +1,45 @@
+package com.radware.vision.devicesRestApi.topologyTree;
+
+import com.radware.vision.RestStepResult;
+
+/**
+ * Created by MohamadI - Muhamad Igbaria
+ * Date: 5/5/2020
+ * Time: 1:33 AM
+ */
+public class TopologyTreeImpl implements TopologyTree {
+    @Override
+    public RestStepResult addDevice(String setId) {
+        return null;
+    }
+
+    @Override
+    public RestStepResult getDevice(String setId) {
+        return null;
+    }
+
+    @Override
+    public RestStepResult updateDevice(String setId) {
+        return null;
+    }
+
+    @Override
+    public RestStepResult deleteDevice(String setId) {
+        return null;
+    }
+
+    @Override
+    public RestStepResult addSite(String siteName, String parentSiteName) {
+        return null;
+    }
+
+    @Override
+    public RestStepResult getSite(String siteName) {
+        return null;
+    }
+
+    @Override
+    public RestStepResult deleteSite(String siteName) {
+        return null;
+    }
+}
