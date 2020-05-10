3f31d8e27dc47a73b19bd22fe1b394e181fc6c24
mohamadi <radware>
2020-05-11 01:14:58 +0300

    Generic CRUD

--- Vision-Auto-Tests/Database-Step-Handlers/src/main/java/com/radware/vision/automation/DatabaseStepHandlers/mariaDB/GenericCRUD.java
+++ Vision-Auto-Tests/Database-Step-Handlers/src/main/java/com/radware/vision/automation/DatabaseStepHandlers/mariaDB/GenericCRUD.java
@@ -1,14 +1,11 @@
 package com.radware.vision.automation.DatabaseStepHandlers.mariaDB;
 
-import com.radware.vision.automation.DatabaseStepHandlers.mariaDB.client.JDBCConnectionException;
 import com.radware.vision.automation.DatabaseStepHandlers.mariaDB.client.JDBCConnectionSingleton;
 import com.radware.vision.automation.DatabaseStepHandlers.mariaDB.client.VisionDBSchema;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Optional;
 
 import static java.lang.String.format;
 
@@ -20,12 +17,15 @@ import static java.lang.String.format;
 public class GenericCRUD {
     private static JDBCConnectionSingleton jdbcConnection = JDBCConnectionSingleton.getInstance();
 
-    public static <T> Optional<T> getOneValue(VisionDBSchema schema, String columnName, String tableName, String where) throws JDBCConnectionException, SQLException {
+    public static <T> T getOneValue(VisionDBSchema schema, String columnName, String tableName, String where) throws Exception {
         Connection dbConnection = jdbcConnection.getDBConnection(schema);
         Statement statement = dbConnection.createStatement();
         ResultSet resultSet = statement.executeQuery(format("SELECT %s FROM %s WHERE %s;", columnName, tableName, where));
+        resultSet.last();
+        if (resultSet.getRow() == 0) throw new Exception("No rows was found with the condition you provide.");
+        if (resultSet.getRow() > 1) throw new Exception("The condition you provide returns more than one row.");
+        T result = (T) resultSet.getObject(1);
 
-
-        return null;
+        return result;
     }
 }
