c23a7fac6ce7cf01abb8c2b70e6e7fc946dec4cf
mohamadi <radware>
2020-05-11 00:29:57 +0300

    Generic CRUD

--- Vision-Auto-Tests/Database-Step-Handlers/src/main/java/com/radware/vision/automation/DatabaseStepHandlers/mariaDB/GenericCRUD.java
+++ Vision-Auto-Tests/Database-Step-Handlers/src/main/java/com/radware/vision/automation/DatabaseStepHandlers/mariaDB/GenericCRUD.java
@@ -1,9 +1,12 @@
 package com.radware.vision.automation.DatabaseStepHandlers.mariaDB;
 
+import com.radware.vision.automation.DatabaseStepHandlers.mariaDB.client.JDBCConnectionSingleton;
+
 /**
  * Created by MohamadI - Muhamad Igbaria
  * Date: 5/11/2020
  * Time: 12:29 AM
  */
 public class GenericCRUD {
+    private static JDBCConnectionSingleton jdbcConnection = JDBCConnectionSingleton.getInstance();
 }
