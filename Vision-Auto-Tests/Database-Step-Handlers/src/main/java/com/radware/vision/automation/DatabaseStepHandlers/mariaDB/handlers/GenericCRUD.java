e920898c085abe51a3a1f4abff6c5f9591ac9dc5
mohamadi <radware>
2020-05-11 00:21:02 +0300

    Revert "Creating Select {column} from {table} where {*}"
    This reverts commit 44cb2efa

--- Vision-Auto-Tests/Database-Step-Handlers/src/main/java/com/radware/vision/automation/DatabaseStepHandlers/mariaDB/handlers/GenericCRUD.java
+++ Vision-Auto-Tests/Database-Step-Handlers/src/main/java/com/radware/vision/automation/DatabaseStepHandlers/mariaDB/handlers/GenericCRUD.java
@@ -1,10 +1,7 @@
 package com.radware.vision.automation.DatabaseStepHandlers.mariaDB.handlers;
 
-import com.radware.vision.automation.DatabaseStepHandlers.mariaDB.client.JDBCConnectionException;
-import com.radware.vision.automation.DatabaseStepHandlers.mariaDB.client.JDBCConnectionSingleton;
 import com.radware.vision.automation.DatabaseStepHandlers.mariaDB.client.VisionDBSchema;
 
-import java.sql.Connection;
 import java.util.Optional;
 
 /**
@@ -14,10 +11,8 @@ import java.util.Optional;
  */
 public class GenericCRUD {
 
-    private static JDBCConnectionSingleton connection = JDBCConnectionSingleton.getInstance();
+    public <T> Optional<T> selectOneValue(String columnName, String fromTable, String where, VisionDBSchema schema) {
 
-    public <T> Optional<T> selectOneValue(String columnName, String fromTable, String where, VisionDBSchema schema) throws JDBCConnectionException {
-        Connection schemaConnection = connection.getDBConnection(schema);
         return null;
     }
 }
