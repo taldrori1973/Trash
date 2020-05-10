8c471203cc7765ecdba696bc1dec36e00a7c401c
mohamadi <radware>
2020-05-10 23:39:46 +0300

    SqlDbConnection

--- Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/repositories/pojos/sut/SqlDbConnection.java
+++ Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/repositories/pojos/sut/SqlDbConnection.java
@@ -1,10 +1,13 @@
 package com.radware.vision.automation.AutoUtils.SUT.repositories.pojos.sut;
 
+import lombok.Data;
+
 /**
  * Created by MohamadI - Muhamad Igbaria
  * Date: 5/10/2020
  * Time: 11:37 PM
  */
+@Data
 public class SqlDbConnection {
 
     private int defaultPort;
