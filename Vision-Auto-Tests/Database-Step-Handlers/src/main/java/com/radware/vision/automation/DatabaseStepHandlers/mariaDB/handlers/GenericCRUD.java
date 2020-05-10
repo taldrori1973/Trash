5ca0033602568f14b6d6968475516848d21ce534
mohamadi <radware>
2020-05-11 00:16:43 +0300

    CRUD

--- Vision-Auto-Tests/Database-Step-Handlers/src/main/java/com/radware/vision/automation/DatabaseStepHandlers/mariaDB/handlers/GenericCRUD.java
+++ Vision-Auto-Tests/Database-Step-Handlers/src/main/java/com/radware/vision/automation/DatabaseStepHandlers/mariaDB/handlers/GenericCRUD.java
@@ -1,9 +1,16 @@
 package com.radware.vision.automation.DatabaseStepHandlers.mariaDB.handlers;
 
+import com.radware.vision.automation.DatabaseStepHandlers.mariaDB.client.VisionDBSchema;
+
 /**
  * Created by MohamadI - Muhamad Igbaria
  * Date: 5/11/2020
  * Time: 12:13 AM
  */
 public class GenericCRUD {
+
+    public <T> T selectOneValue(String columnName, String fromTable, String where, VisionDBSchema schema) {
+
+        return null;
+    }
 }
