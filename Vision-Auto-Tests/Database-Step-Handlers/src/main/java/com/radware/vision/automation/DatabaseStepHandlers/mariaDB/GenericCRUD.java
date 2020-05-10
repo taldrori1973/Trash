b213bc4684e122edf7d39fc61d3376e02874a514
mohamadi <radware>
2020-05-11 00:29:31 +0300

    Generic CRUD

---
+++ Vision-Auto-Tests/Database-Step-Handlers/src/main/java/com/radware/vision/automation/DatabaseStepHandlers/mariaDB/GenericCRUD.java
@@ -0,0 +1,9 @@
+package com.radware.vision.automation.DatabaseStepHandlers.mariaDB;
+
+/**
+ * Created by MohamadI - Muhamad Igbaria
+ * Date: 5/11/2020
+ * Time: 12:29 AM
+ */
+public class GenericCRUD {
+}
