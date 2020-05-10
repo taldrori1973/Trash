cb951ba5fcc4abd0ca7d3dd9f6cd993f65697c4e
mohamadi <radware>
2020-05-11 00:31:38 +0300

    Generic CRUD

--- Vision-Auto-Tests/Database-Step-Handlers/src/main/java/com/radware/vision/automation/DatabaseStepHandlers/mariaDB/GenericCRUD.java
+++ Vision-Auto-Tests/Database-Step-Handlers/src/main/java/com/radware/vision/automation/DatabaseStepHandlers/mariaDB/GenericCRUD.java
@@ -1,7 +1,10 @@
 package com.radware.vision.automation.DatabaseStepHandlers.mariaDB;
 
+import com.radware.vision.automation.DatabaseStepHandlers.mariaDB.client.JDBCConnectionException;
 import com.radware.vision.automation.DatabaseStepHandlers.mariaDB.client.JDBCConnectionSingleton;
+import com.radware.vision.automation.DatabaseStepHandlers.mariaDB.client.VisionDBSchema;
 
+import java.sql.Connection;
 import java.util.Optional;
 
 /**
@@ -12,8 +15,8 @@ import java.util.Optional;
 public class GenericCRUD {
     private static JDBCConnectionSingleton jdbcConnection = JDBCConnectionSingleton.getInstance();
 
-    public <T>Optional<T> getOneValue(){
-
+    public <T> Optional<T> getOneValue(VisionDBSchema schema) throws JDBCConnectionException {
+        Connection dbConnection = jdbcConnection.getDBConnection(schema);
         return null;
     }
 }
