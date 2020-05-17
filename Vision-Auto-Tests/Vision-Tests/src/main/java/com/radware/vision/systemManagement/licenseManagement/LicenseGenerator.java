a18b2c2e0cc5114478d732648a5bdcf45d50ec85
mohamadi <radware>
2020-05-18 01:03:07 +0300

    Move Vision Management

--- Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/systemManagement/licenseManagement/LicenseGenerator.java
+++ Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/systemManagement/licenseManagement/LicenseGenerator.java
@@ -1,5 +1,6 @@
 package com.radware.vision.systemManagement.licenseManagement;
 
+import com.radware.vision.base.TestBase;
 import com.radware.vision.utils.ReflectionUtils;
 
 /**
@@ -7,11 +8,18 @@ import com.radware.vision.utils.ReflectionUtils;
  * Copied to this Project and modified by MohamadI on 30/03/2020
  */
 public class LicenseGenerator {
+    private static String MAC_ADDRESS = TestBase.getVisionConfigurations().getManagementInfo().getMacAddress();
+
     public static String generateVisionActivationLicenseKey(String macAddress) throws Exception {
         String licenseKey;
         String licensePrefix = VisionLicenses.ACTIVATION.getLicensePrefixPattern();
         licenseKey = licensePrefix + "-" + ReflectionUtils.invokePrivateMethod("generateLicenseString", macAddress, licensePrefix);
         return licenseKey;
+    }
+
+
+    public static String generateLicense(String licenseKeyPrefix) throws Exception {
+        return generateLicense(MAC_ADDRESS, licenseKeyPrefix);
     }
 
     public static String generateLicense(String macAddress, String licenseKeyPrefix) throws Exception {
