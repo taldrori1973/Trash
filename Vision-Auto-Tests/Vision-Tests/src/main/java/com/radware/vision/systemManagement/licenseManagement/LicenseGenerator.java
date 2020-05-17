2d00a0d56a76a94c3c88ed3ee590f5c06bdd6fcd
mohamadi <radware>
2020-05-18 01:00:50 +0300

    Move Vision Management

--- Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/systemManagement/licenseManagement/LicenseGenerator.java
+++ Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/systemManagement/licenseManagement/LicenseGenerator.java
@@ -1,12 +1,6 @@
 package com.radware.vision.systemManagement.licenseManagement;
 
-import com.radware.jsonparsers.impl.GenericJSonParsers;
-import com.radware.restcore.VisionRestClient;
-import com.radware.vision.pojomodel.security.VisionLicensePojo;
 import com.radware.vision.utils.ReflectionUtils;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by ashrafa on 7/4/2017.
@@ -20,32 +14,4 @@ public class LicenseGenerator {
         return licenseKey;
     }
 
-
-    private static String getFromToPeriod(String fromDate, String toDate) {
-        String fromToPeriod = "";
-        if (!fromDate.equals("") && !toDate.equals("")) {
-            fromToPeriod = ("-").concat(fromDate).concat("-").concat(toDate);
-        }
-
-        return fromToPeriod;
-    }
-
-    public static List<String> getLicenses(VisionRestClient visionRestClient) {
-        visionRestClient.login();
-        String result = visionRestClient.mgmtCommands.visionCommands.getVisionLicense();
-        List<VisionLicensePojo> licensePojo = GenericJSonParsers.getInstance().parseJsonList(result, VisionLicensePojo.class);
-        List<String> licenseStr = new ArrayList<>();
-
-        for (VisionLicensePojo license : licensePojo) {
-            licenseStr.add(license.getLicenseStr());
-        }
-        return licenseStr;
-    }
-
-    public static boolean isInstalled(VisionRestClient visionRestClient, String licensePrefix) {
-
-        return getLicenses(visionRestClient).stream()
-                .filter(license -> license.startsWith(licensePrefix))
-                .findFirst().isPresent();
-    }
 }
