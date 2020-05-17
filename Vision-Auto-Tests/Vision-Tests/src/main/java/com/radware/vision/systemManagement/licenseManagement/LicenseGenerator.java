dab471f377869d69d91f64a017d5cfc4360428d8
mohamadi <radware>
2020-05-18 01:13:52 +0300

    Move Vision Management

--- Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/systemManagement/licenseManagement/LicenseGenerator.java
+++ Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/systemManagement/licenseManagement/LicenseGenerator.java
@@ -11,41 +11,39 @@ public class LicenseGenerator {
     private static String MAC_ADDRESS = TestBase.getVisionConfigurations().getManagementInfo().getMacAddress();
 
     /**
-     *
      * @return Vision Activation License Key by the default MAC Address from TestBase.getVisionConfigurations().getManagementInfo().getMacAddress()
      */
-    public static String generateVisionActivationLicenseKey(){
+    public static String generateVisionActivationLicenseKey() {
         return generateVisionActivationLicenseKey(MAC_ADDRESS);
     }
+
     /**
-     *
-     * @param macAddress    Vision Mac Address
-     * @return              Vision Activation License Key
+     * @param macAddress Vision Mac Address
+     * @return Vision Activation License Key
      * @throws Exception
      */
     public static String generateVisionActivationLicenseKey(String macAddress) {
 
-        return generateLicense(macAddress,VisionLicenses.ACTIVATION.getLicensePrefixPattern());
+        return generateLicense(macAddress, VisionLicenses.ACTIVATION.getLicensePrefixPattern());
     }
 
 
     /**
-     *
      * @param licenseKeyPrefix could be one of two formats:
      *                         time based license : vision-AVA-6-Gbps-attack-capacity-03sep2019-29oct2020
-     *                          when the vision-AVA-6-Gbps-attack-capacity you can get from {@link VisionLicenses} and the from to dates are added with the format above.
-     *
+     *                              when the vision-AVA-6-Gbps-attack-capacity you can get from {@link VisionLicenses} and the from to dates are added with the format above.
+     *                         <p>
      *                         Or
-     *
+     *                         <p>
      *                         not time based license :vision-AVA-400-Gbps-attack-capacity
-     *                              *                          when the vision-AVA-6-Gbps-attack-capacity you can get from {@link VisionLicenses} without the from to dates.
-     * @return      licenseKeyPrefix License Key by the default MAC Address from TestBase.getVisionConfigurations().getManagementInfo().getMacAddress()
+     *                              when the vision-AVA-6-Gbps-attack-capacity you can get from {@link VisionLicenses} without the from to dates.
+     * @return licenseKeyPrefix License Key by the default MAC Address from TestBase.getVisionConfigurations().getManagementInfo().getMacAddress()
      */
-    public static String generateLicense(String licenseKeyPrefix)  {
+    public static String generateLicense(String licenseKeyPrefix) {
         return generateLicense(MAC_ADDRESS, licenseKeyPrefix);
     }
 
-    public static String generateLicense(String macAddress, String licenseKeyPrefix)  {
+    public static String generateLicense(String macAddress, String licenseKeyPrefix) {
         String licenseKey;
         licenseKey = licenseKeyPrefix + "-" + ReflectionUtils.invokePrivateMethod("generateLicenseString", macAddress, licenseKeyPrefix);
         return licenseKey;
