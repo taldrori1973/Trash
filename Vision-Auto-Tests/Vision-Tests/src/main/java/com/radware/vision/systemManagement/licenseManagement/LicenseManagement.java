468fff11fb8145bae5d0a93dff3aaed73db317a5
mohamadi <radware>
2020-05-18 02:13:07 +0300

    License Management

--- Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/systemManagement/licenseManagement/LicenseManagement.java
+++ Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/systemManagement/licenseManagement/LicenseManagement.java
@@ -206,7 +206,7 @@ public class LicenseManagement {
 
         if (licenseToDelete.isPresent())
             return restDelete(licenseToDelete.get());
-        else return false;
+        return false;
 
     }
 
