daf789e85646fcd0a999295218890428e52f4ed3
mohamadi <mohamadi@radware.com>
2020-06-25 13:54:28 +0300

    Pojos

--- Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/thirdPartyAPIs/jFrog/pojos/ArtifactChildPojo.java
+++ Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/thirdPartyAPIs/jFrog/pojos/ArtifactChildPojo.java
@@ -1,10 +1,13 @@
 package com.radware.vision.thirdPartyAPIs.jFrog.pojos;
 
+import lombok.Data;
+
 /**
  * Created by MohamadI - Muhamad Igbaria
  * Date: 6/25/2020
  * Time: 1:52 PM
  */
+@Data
 public class ArtifactChildPojo {
 
     private String uri;
