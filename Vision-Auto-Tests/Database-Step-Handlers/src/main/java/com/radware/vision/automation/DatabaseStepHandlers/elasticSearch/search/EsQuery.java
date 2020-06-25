04994302b948c009327b0c090d08ee6cc498bcaf
RamezH <ramezh@radware.com>
2020-06-25 16:17:41 +0300

    EsQuery root pojo

---
+++ Vision-Auto-Tests/Database-Step-Handlers/src/main/java/com/radware/vision/automation/DatabaseStepHandlers/elasticSearch/search/EsQuery.java
@@ -0,0 +1,9 @@
+package com.radware.vision.automation.DatabaseStepHandlers.elasticSearch.search;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class EsQuery {
+    SearchQuery query;
+}
