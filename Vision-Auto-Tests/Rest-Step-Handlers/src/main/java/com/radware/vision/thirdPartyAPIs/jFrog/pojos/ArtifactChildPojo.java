b665a1d90b573dbef2cfcced46cb613b2cd223c3
mohamadi <mohamadi@radware.com>
2020-06-25 13:53:07 +0300

    Pojos

---
+++ Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/thirdPartyAPIs/jFrog/pojos/ArtifactChildPojo.java
@@ -0,0 +1,9 @@
+package com.radware.vision.thirdPartyAPIs.jFrog.pojos;
+
+/**
+ * Created by MohamadI - Muhamad Igbaria
+ * Date: 6/25/2020
+ * Time: 1:52 PM
+ */
+public class ArtifactChildPojo {
+}
--- Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/thirdPartyAPIs/jFrog/pojos/ArtifactPojo.java
+++ Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/thirdPartyAPIs/jFrog/pojos/ArtifactPojo.java
@@ -2,6 +2,9 @@ package com.radware.vision.thirdPartyAPIs.jFrog.pojos;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
+import org.joda.time.DateTime;
+
+import java.util.List;
 
 /**
  * Created by MohamadI - Muhamad Igbaria
@@ -11,4 +14,12 @@ import lombok.Data;
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ArtifactPojo {
+
+    private String repo;
+    private String path;
+    private DateTime created;
+    private DateTime lastModified;
+    private DateTime lastUpdated;
+    private List<ArtifactChildPojo> children;
+    private String uri;
 }
