c3dcbd9c1d68a524adfbb84c355c50bc6a2845f7
mohamadi <mohamadi@radware.com>
2020-06-25 15:28:31 +0300

    Pojos

--- Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/thirdPartyAPIs/jFrog/models/ArtifactModel.java
+++ Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/thirdPartyAPIs/jFrog/models/ArtifactModel.java
@@ -1,23 +1,17 @@
 package com.radware.vision.thirdPartyAPIs.jFrog.models;
 
-import lombok.Data;
-
 import java.net.URI;
 import java.util.Date;
-import java.util.List;
 
 /**
  * Created by MohamadI - Muhamad Igbaria
  * Date: 6/25/2020
- * Time: 1:42 PM
+ * Time: 3:27 PM
  */
-@Data
-public class Artifact extends ArtifactModel {
-    private String name;
+public class ArtifactModel {
     private String repo;
     private URI path;
     private Date created;
     private Date lastModified;
     private Date lastUpdated;
-    private List<ArtifactVersion> versions;
 }
