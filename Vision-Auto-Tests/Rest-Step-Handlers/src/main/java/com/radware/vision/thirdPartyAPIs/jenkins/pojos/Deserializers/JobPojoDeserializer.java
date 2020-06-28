247f4dceaa6c4063971c2e31f818bc743d315cd6
mohamadi <mohamadi@radware.com>
2020-06-28 16:34:09 +0300

    Pojos

--- Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/thirdPartyAPIs/jenkins/pojos/Deserializers/JobPojoDeserializer.java
+++ Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/thirdPartyAPIs/jenkins/pojos/Deserializers/JobPojoDeserializer.java
@@ -1,12 +1,28 @@
 package com.radware.vision.thirdPartyAPIs.jenkins.pojos.Deserializers;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.radware.vision.thirdPartyAPIs.jenkins.pojos.JobPojo;
+
+import java.io.IOException;
+
 /**
  * Created by MohamadI - Muhamad Igbaria
  * Date: 6/25/2020
  * Time: 1:42 PM
  */
 
-public class JobPojoDeserializer {
+public class JobPojoDeserializer extends StdDeserializer<JobPojo> {
 
 
+    protected JobPojoDeserializer(Class<?> vc) {
+        super(vc);
+    }
+
+    @Override
+    public JobPojo deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
+        return null;
+    }
 }
