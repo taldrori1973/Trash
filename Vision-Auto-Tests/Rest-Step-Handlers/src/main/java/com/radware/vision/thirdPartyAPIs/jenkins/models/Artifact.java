bd23707bfcca9aa2af4fd106ed425dd84114c57d
mohamadi <mohamadi@radware.com>
2020-06-28 15:33:51 +0300

    Pojos

--- Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/thirdPartyAPIs/jenkins/models/Artifact.java
+++ Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/thirdPartyAPIs/jenkins/models/Artifact.java
@@ -2,15 +2,12 @@ package com.radware.vision.thirdPartyAPIs.jenkins.models;
 
 import lombok.Data;
 
-import java.util.List;
-
 /**
  * Created by MohamadI - Muhamad Igbaria
  * Date: 6/25/2020
  * Time: 1:42 PM
  */
 @Data
-public class Artifact extends ArtifactModel {
+public class Artifact  {
     private String name;
-    private List<ArtifactVersion> versions;
 }
