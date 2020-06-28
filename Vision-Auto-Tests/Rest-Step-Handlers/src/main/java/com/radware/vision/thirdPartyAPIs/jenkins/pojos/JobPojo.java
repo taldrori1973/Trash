931556320b9abcfae3ce34d5b65a5242ab658d78
mohamadi <mohamadi@radware.com>
2020-06-28 17:00:45 +0300

    Pojos

--- Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/thirdPartyAPIs/jenkins/pojos/JobPojo.java
+++ Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/thirdPartyAPIs/jenkins/pojos/JobPojo.java
@@ -2,6 +2,7 @@ package com.radware.vision.thirdPartyAPIs.jenkins.pojos;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.radware.vision.thirdPartyAPIs.jenkins.pojos.Deserializers.JobPojoBuildDeserializer;
 import com.radware.vision.thirdPartyAPIs.jenkins.pojos.Deserializers.JobPojoBuildsArrayDeserializer;
 import lombok.Data;
 
@@ -19,13 +20,19 @@ public class JobPojo {
     private String name;
     private Integer nextBuildNumber;
 
-    @JsonDeserialize(using = JobPojoBuildsArrayDeserializer.class)
+    @JsonDeserialize(using = JobPojoBuildDeserializer.class)
     private Integer firstBuild;
+    @JsonDeserialize(using = JobPojoBuildDeserializer.class)
     private Integer lastBuild;
+    @JsonDeserialize(using = JobPojoBuildDeserializer.class)
     private Integer lastCompletedBuild;
+    @JsonDeserialize(using = JobPojoBuildDeserializer.class)
     private Integer lastFailedBuild;
+    @JsonDeserialize(using = JobPojoBuildDeserializer.class)
     private Integer lastStableBuild;
+    @JsonDeserialize(using = JobPojoBuildDeserializer.class)
     private Integer lastSuccessfulBuild;
+    @JsonDeserialize(using = JobPojoBuildDeserializer.class)
     private Integer lastUnstableBuild;
 
     @JsonDeserialize(using = JobPojoBuildsArrayDeserializer.class)
