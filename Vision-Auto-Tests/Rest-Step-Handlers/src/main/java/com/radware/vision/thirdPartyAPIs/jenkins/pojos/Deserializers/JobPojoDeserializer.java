a3d6143573b6219e17db364afb0671de4f3a1c4e
mohamadi <mohamadi@radware.com>
2020-06-28 16:32:47 +0300

    Pojos

--- Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/thirdPartyAPIs/jenkins/pojos/Deserializers/JobPojoDeserializer.java
+++ Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/thirdPartyAPIs/jenkins/pojos/Deserializers/JobPojoDeserializer.java
@@ -1,31 +1,27 @@
 package com.radware.vision.thirdPartyAPIs.jenkins.pojos.Deserializers;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
-import com.radware.vision.thirdPartyAPIs.jenkins.pojos.JobPojo;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
 
-import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by MohamadI - Muhamad Igbaria
  * Date: 6/25/2020
  * Time: 1:42 PM
  */
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class JobPojoDeserializer {
 
-public class JobPojoDeserializer extends StdDeserializer<JobPojo> {
-
-    protected JobPojoDeserializer() {
-        this(null);
-    }
-
-    protected JobPojoDeserializer(Class<?> vc) {
-        super(vc);
-    }
-
-    @Override
-    public JobPojo deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
-        return null;
-    }
+    private String name;
+    private Integer firstBuild;
+    private Integer lastBuild;
+    private Integer lastCompletedBuild;
+    private Integer lastFailedBuild;
+    private Integer lastStableBuild;
+    private Integer lastSuccessfulBuild;
+    private Integer lastUnstableBuild;
+    private Integer nextBuildNumber;
+    private List<Integer> builds;
 }
