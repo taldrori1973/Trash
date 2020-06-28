669d43d553cc0e9fd4c1d90940232139df2940b9
mohamadi <mohamadi@radware.com>
2020-06-28 17:39:14 +0300

    Pojos

---
+++ Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/thirdPartyAPIs/jenkins/models/JenkinsAPI.java
@@ -0,0 +1,22 @@
+package com.radware.vision.thirdPartyAPIs.jenkins.models;
+
+import com.radware.vision.thirdPartyAPIs.jenkins.pojos.BuildPojo;
+import com.radware.vision.thirdPartyAPIs.jenkins.pojos.JobPojo;
+import lombok.Data;
+
+/**
+ * Created by MohamadI - Muhamad Igbaria
+ * Date: 6/25/2020
+ * Time: 1:42 PM
+ */
+@Data
+public class JenkinsAPI {
+    public JobPojo getJobInfo(String jobName) {
+
+        return null;
+    }
+
+    public BuildPojo getBuildInfo(String jobName, Integer buildNumber) {
+        return null
+    }
+}
