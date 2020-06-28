291fc6b4ad6aec376925da8af9a308ef360a880b
mohamadi <mohamadi@radware.com>
2020-06-28 16:29:56 +0300

    Pojos

--- Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/thirdPartyAPIs/jenkins/pojos/Deserializers/JobPojoDeserializer.java
+++ Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/thirdPartyAPIs/jenkins/pojos/Deserializers/JobPojoDeserializer.java
@@ -1,12 +1,10 @@
 package com.radware.vision.thirdPartyAPIs.jenkins.pojos.Deserializers;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import com.radware.vision.thirdPartyAPIs.jenkins.pojos.JobPojo;
-import lombok.Data;
 
 import java.io.IOException;
 
@@ -15,10 +13,16 @@ import java.io.IOException;
  * Date: 6/25/2020
  * Time: 1:42 PM
  */
-@Data
-@JsonIgnoreProperties(ignoreUnknown = true)
+
 public class JobPojoDeserializer extends StdDeserializer<JobPojo> {
 
+    protected JobPojoDeserializer() {
+        this(null);
+    }
+
+    protected JobPojoDeserializer(Class<?> vc) {
+        super(vc);
+    }
 
     @Override
     public JobPojo deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
