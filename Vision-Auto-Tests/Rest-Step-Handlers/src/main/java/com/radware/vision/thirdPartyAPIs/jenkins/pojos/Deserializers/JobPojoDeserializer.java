8b239c8cbc3136ebe389083414e1058c4a99e22f
mohamadi <mohamadi@radware.com>
2020-06-28 16:33:10 +0300

    Pojos

--- Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/thirdPartyAPIs/jenkins/pojos/Deserializers/JobPojoDeserializer.java
+++ Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/thirdPartyAPIs/jenkins/pojos/Deserializers/JobPojoDeserializer.java
@@ -1,27 +1,12 @@
 package com.radware.vision.thirdPartyAPIs.jenkins.pojos.Deserializers;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Data;
-
-import java.util.List;
-
 /**
  * Created by MohamadI - Muhamad Igbaria
  * Date: 6/25/2020
  * Time: 1:42 PM
  */
-@Data
-@JsonIgnoreProperties(ignoreUnknown = true)
+
 public class JobPojoDeserializer {
 
-    private String name;
-    private Integer firstBuild;
-    private Integer lastBuild;
-    private Integer lastCompletedBuild;
-    private Integer lastFailedBuild;
-    private Integer lastStableBuild;
-    private Integer lastSuccessfulBuild;
-    private Integer lastUnstableBuild;
-    private Integer nextBuildNumber;
-    private List<Integer> builds;
+
 }
