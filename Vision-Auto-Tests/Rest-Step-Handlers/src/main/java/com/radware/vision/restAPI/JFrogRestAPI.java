08507ea9fd6da48eb228eeff01bbb9f956f5dafa
mohamadi <mohamadi@radware.com>
2020-06-29 15:49:26 +0300

    Pojos

--- Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/restAPI/JFrogRestAPI.java
+++ Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/restAPI/JFrogRestAPI.java
@@ -1,7 +1,9 @@
 package com.radware.vision.restAPI;
 
 import com.radware.vision.RestClientsFactory;
+import com.radware.vision.automation.AutoUtils.utils.ApplicationPropertiesUtils;
 import models.RestResponse;
+import restInterface.client.NoAuthRestClient;
 
 /**
  * Created by MohamadI - Muhamad Igbaria
@@ -10,13 +12,27 @@ import models.RestResponse;
  */
 public class JFrogRestAPI {
 
+    private static ApplicationPropertiesUtils applicationPropertiesUtils = new ApplicationPropertiesUtils();
     private String baseUri;
     private Integer connectionPort;
+    private String artifactName;
 
-    public JFrogRestAPI(){
-        this
+    public JFrogRestAPI(String artifactName) {
+        this.baseUri = String.format(
+                "%s://%s",
+                applicationPropertiesUtils.getProperty("JFrog.artifactory.protocol"),
+                applicationPropertiesUtils.getProperty("JFrog.artifactory.host")
+        );
+
+        this.connectionPort = Integer.parseInt(applicationPropertiesUtils.getProperty("JFrog.artifactory.port"));
+
+        this.artifactName = artifactName;
+
     }
-    public RestResponse sendRequest(){
-        RestClientsFactory.getNoAuthConnection()
+
+    public RestResponse sendRequest() {
+        NoAuthRestClient connection = RestClientsFactory.getNoAuthConnection(this.baseUri, this.connectionPort);
+        connection.switchTo();
+
     }
 }
