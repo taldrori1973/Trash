330ae36c96f5cdfaf6be2b1fe03da0f1f04a296b
mohamadi <mohamadi@radware.com>
2020-06-29 17:03:28 +0300

    Pojos

--- Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/thirdPartyAPIs/jFrog/ArtifactService.java
+++ Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/thirdPartyAPIs/jFrog/ArtifactService.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.radware.vision.restAPI.JFrogRestAPI;
 import com.radware.vision.thirdPartyAPIs.jFrog.models.Artifact;
 import com.radware.vision.thirdPartyAPIs.jFrog.models.FileType;
+import com.radware.vision.thirdPartyAPIs.jFrog.pojos.ArtifactFolderPojo;
 import com.radware.vision.thirdPartyAPIs.jFrog.pojos.ArtifactPojo;
 import models.RestResponse;
 import models.StatusCode;
@@ -40,6 +41,10 @@ public class ArtifactService {
     }
 
     public void getBuild(FileType fileType, String repoName, String version, String branch, Integer build) {
-
+        JFrogRestAPI jFrogRestAPI=new JFrogRestAPI(repoName);
+        if(!version.equals("Latest")) {//go to the specific version folder
+            RestResponse restResponse = jFrogRestAPI.sendRequest(version);
+            ArtifactFolderPojo versionPojo=objectMapper.readValue(restResponse.getBody().getBodyAsString(),ArtifactFolderPojo.class);
+        }
     }
 }
