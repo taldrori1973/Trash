3c7b7a7ea62052e587b718d516a18d85219937c0
mohamadi <mohamadi@radware.com>
2020-06-29 17:07:41 +0300

    Pojos

--- Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/thirdPartyAPIs/jFrog/ArtifactService.java
+++ Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/thirdPartyAPIs/jFrog/ArtifactService.java
@@ -1,6 +1,5 @@
 package com.radware.vision.thirdPartyAPIs.jFrog;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.radware.vision.restAPI.JFrogRestAPI;
 import com.radware.vision.thirdPartyAPIs.jFrog.models.Artifact;
@@ -26,14 +25,14 @@ public class ArtifactService {
     public Artifact getArtifact(String repoName) throws Exception {
 
         JFrogRestAPI jFrogRestAPI = new JFrogRestAPI(repoName);
-        RestResponse repoResponse = jFrogRestAPI.sendRequest("",StatusCode.OK);
+        RestResponse repoResponse = jFrogRestAPI.sendRequest("", StatusCode.OK);
 
         if (!repoResponse.getStatusCode().equals(StatusCode.OK))
             throw new Exception(repoResponse.getBody().getBodyAsString());
 
-        ArtifactPojo artifactPojo=this.objectMapper.readValue(repoResponse.getBody().getBodyAsString(),ArtifactPojo.class);
+        ArtifactPojo artifactPojo = this.objectMapper.readValue(repoResponse.getBody().getBodyAsString(), ArtifactPojo.class);
 
-        Artifact artifact=mapPojoToModel(artifactPojo);
+        Artifact artifact = mapPojoToModel(artifactPojo);
         return null;
     }
 
@@ -41,11 +40,16 @@ public class ArtifactService {
         return null;
     }
 
-    public void getBuild(FileType fileType, String repoName, String version, String branch, Integer build) throws JsonProcessingException {
-        JFrogRestAPI jFrogRestAPI=new JFrogRestAPI(repoName);
-        if(!version.equals("Latest")) {//go to the specific version folder
-            RestResponse restResponse = jFrogRestAPI.sendRequest(version,StatusCode.OK);
-            ArtifactFolderPojo versionPojo=objectMapper.readValue(restResponse.getBody().getBodyAsString(),ArtifactFolderPojo.class);
+    public void getBuild(FileType fileType, String repoName, String version, String branch, Integer build) throws Exception {
+        ArtifactFolderPojo versionPojo;
+        ArtifactFolderPojo branchPojo;
+        ArtifactFolderPojo buildPojo;
+
+        JFrogRestAPI jFrogRestAPI = new JFrogRestAPI(repoName);
+        if (!version.equals("Latest")) {//go to the specific version folder
+            RestResponse restResponse = jFrogRestAPI.sendRequest(version, StatusCode.OK);
+            versionPojo = objectMapper.readValue(restResponse.getBody().getBodyAsString(), ArtifactFolderPojo.class);
         }
+
     }
 }
