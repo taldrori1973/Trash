d053b1b02b7eb7bda422a3194f62f23a64aefb53
mohamadi <mohamadi@radware.com>
2020-06-29 15:19:37 +0300

    Pojos

---
+++ Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/thirdPartyAPIs/jFrog/models/Child.java
@@ -0,0 +1,13 @@
+package com.radware.vision.thirdPartyAPIs.jFrog.models;
+
+import lombok.Data;
+
+/**
+ * Created by MohamadI - Muhamad Igbaria
+ * Date: 6/29/2020
+ * Time: 3:18 PM
+ */
+@Data
+public class Child {
+
+}
