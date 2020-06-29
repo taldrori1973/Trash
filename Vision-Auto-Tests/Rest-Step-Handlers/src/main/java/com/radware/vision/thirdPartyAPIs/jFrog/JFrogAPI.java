83ec1c137afcdaf7dbdd9ad6e01bc8062b81159d
mohamadi <mohamadi@radware.com>
2020-06-29 15:34:43 +0300

    Pojos

---
+++ Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/thirdPartyAPIs/jFrog/JFrogAPI.java
@@ -0,0 +1,15 @@
+package com.radware.vision.thirdPartyAPIs.jFrog;
+
+import com.radware.vision.thirdPartyAPIs.jFrog.models.Artifact;
+
+/**
+ * Created by MohamadI - Muhamad Igbaria
+ * Date: 6/29/2020
+ * Time: 3:33 PM
+ */
+public class JFrogAPI {
+
+    public static Artifact getArtifact(String artifactName){
+        return null;
+    }
+}
