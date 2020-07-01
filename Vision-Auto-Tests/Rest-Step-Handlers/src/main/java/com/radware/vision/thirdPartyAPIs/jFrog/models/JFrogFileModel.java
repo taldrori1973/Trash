5281a2f7461862e00df30be5d0f06e8e1bdb653a
mohamadi <mohamadi@radware.com>
2020-07-01 13:58:09 +0300

    Jfrog API

--- Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/thirdPartyAPIs/jFrog/models/JFrogFileModel.java
+++ Vision-Auto-Tests/Rest-Step-Handlers/src/main/java/com/radware/vision/thirdPartyAPIs/jFrog/models/JFrogFileModel.java
@@ -4,7 +4,6 @@ import lombok.Data;
 
 import java.net.URI;
 import java.util.Date;
-import java.util.List;
 
 /**
  * Created by MohamadI - Muhamad Igbaria
@@ -12,15 +11,13 @@ import java.util.List;
  * Time: 3:18 PM
  */
 @Data
-public class JFrogFileModel extends ArtifactModel {
+public class JFrogFileModel  {
     private String repo;
     private URI path;
     private Date created;
     private Date lastModified;
     private Date lastUpdated;
     private URI uri;
-    private List<JFrogFileModel> JFrogFileModels;
-    private boolean isFolder;
     private FileType type;
     private URI downloadUri;
     private String size;
