90a5cb6c64ac8fe80248a048a1551658e7596d9a
RamezH <ramezh@radware.com>
2020-07-02 15:59:07 +0300

    updateTimeRange

--- Vision-Auto-Tests/Database-Step-Handlers/src/main/java/com/radware/vision/automation/DatabaseStepHandlers/elasticSearch/LogsHandler.java
+++ Vision-Auto-Tests/Database-Step-Handlers/src/main/java/com/radware/vision/automation/DatabaseStepHandlers/elasticSearch/LogsHandler.java
@@ -3,7 +3,6 @@ package com.radware.vision.automation.DatabaseStepHandlers.elasticSearch;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.radware.vision.automation.DatabaseStepHandlers.elasticSearch.search.EsQuery;
-import com.radware.vision.automation.DatabaseStepHandlers.elasticSearch.search.SearchLog;
 import com.radware.vision.automation.DatabaseStepHandlers.elasticSearch.search.innerQuery.Match;
 import com.radware.vision.automation.DatabaseStepHandlers.elasticSearch.search.SearchBool;
 import com.radware.vision.automation.DatabaseStepHandlers.elasticSearch.search.SearchQuery;
@@ -11,12 +10,11 @@ import com.radware.vision.automation.DatabaseStepHandlers.elasticSearch.search.i
 import com.radware.vision.automation.DatabaseStepHandlers.elasticSearch.search.innerQuery.TimeStamp;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.List;
 
 public class LogsHandler {
     /**
      * check log with containerName if contains a message
+     *
      * @param containerName
      * @param message
      * @return
@@ -30,12 +28,25 @@ public class LogsHandler {
         matchMessage.add("message", message);
         searchBool.getMust().add(matchMessage);
         SearchQuery searchQuery = new SearchQuery(searchBool);
-        EsQuery esQuery= new EsQuery(searchQuery);
+        EsQuery esQuery = new EsQuery(searchQuery);
         ObjectMapper mapper = new ObjectMapper();
         String query = mapper.writeValueAsString(esQuery);
-        ElasticSearchHandler.searchESIndexByQuery("logstash",query,"Hits:0");
+        ElasticSearchHandler.searchESIndexByQuery("logstash", query, "Hits:0");
         return true;
     }
 
+    /**
+     * update the query with test starting time
+     *
+     * @param searchBool the orignal query
+     */
+    public static void updateTimeRange(SearchBool searchBool, LocalDateTime testTime) {
+//         = TestBase.getTestStartTime();
+        testTime = testTime.minusMonths(2);            //////////////////////////temproray
+        TimeStamp timeStamp = new TimeStamp();
+        timeStamp.addFilter("gt", testTime.toString());
+        Range range = new Range(timeStamp);
+        searchBool.getMust().add(range);
+    }
 
 }
