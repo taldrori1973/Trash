ed7752c4bfd0b8f23b0335e7e21634db1ca14472
ramezh <Ramezh@radware.com>
2020-07-05 09:50:44 +0300

    functions moved time -2 months commented

--- Vision-Auto-Tests/Database-Step-Handlers/src/main/java/com/radware/vision/automation/DatabaseStepHandlers/elasticSearch/LogsHandler.java
+++ Vision-Auto-Tests/Database-Step-Handlers/src/main/java/com/radware/vision/automation/DatabaseStepHandlers/elasticSearch/LogsHandler.java
@@ -3,6 +3,7 @@ package com.radware.vision.automation.DatabaseStepHandlers.elasticSearch;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.radware.vision.automation.DatabaseStepHandlers.elasticSearch.search.EsQuery;
+import com.radware.vision.automation.DatabaseStepHandlers.elasticSearch.search.SearchLog;
 import com.radware.vision.automation.DatabaseStepHandlers.elasticSearch.search.innerQuery.Match;
 import com.radware.vision.automation.DatabaseStepHandlers.elasticSearch.search.SearchBool;
 import com.radware.vision.automation.DatabaseStepHandlers.elasticSearch.search.SearchQuery;
@@ -10,6 +11,7 @@ import com.radware.vision.automation.DatabaseStepHandlers.elasticSearch.search.i
 import com.radware.vision.automation.DatabaseStepHandlers.elasticSearch.search.innerQuery.TimeStamp;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class LogsHandler {
     /**
@@ -41,12 +43,50 @@ public class LogsHandler {
      * @param searchBool the orignal query
      */
     public static void updateTimeRange(SearchBool searchBool, LocalDateTime testTime) {
-//         = TestBase.getTestStartTime();
-        testTime = testTime.minusMonths(2);            //////////////////////////temproray
+//        testTime = testTime.minusMonths(2);            //////////////////////////temproray
         TimeStamp timeStamp = new TimeStamp();
         timeStamp.addFilter("gt", testTime.toString());
         Range range = new Range(timeStamp);
         searchBool.getMust().add(range);
     }
+
+    /**
+     * @param selection  the experssion in the log we need to search
+     * @param myIgnored  the ignoreLIst
+     * @param searchBool the orignal query
+     * @return return the query after adding the experssion to it
+     * @throws JsonProcessingException JsonProcessingException
+     */
+    public static String isNotExpectedQuery(SearchLog selection, List<SearchLog> myIgnored, SearchBool searchBool) throws JsonProcessingException {
+        Match mustMatch = new Match();
+        mustMatch.add("message", selection.getExpression());
+        searchBool.getMust().add(mustMatch);
+        for (SearchLog ignore : myIgnored) {
+            if (ignore.getLogType().serverLogType.equalsIgnoreCase(selection.getLogType().serverLogType)) {
+                Match mustNotMatch = new Match();
+                mustNotMatch.add("message", ignore.getExpression());
+                searchBool.getMust_not().add(mustNotMatch);
+            }
+        }
+        EsQuery esQuery = new EsQuery(new SearchQuery(searchBool));
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(esQuery);
+    }
+
+    /**
+     * @param selection  the experssion in the log we need to search
+     * @param searchBool the orignal query
+     * @return return the query after adding the experssion to it
+     * @throws JsonProcessingException
+     */
+    public static String ExpectedQuery(SearchLog selection, SearchBool searchBool) throws JsonProcessingException {
+        Match mustMatch = new Match();
+        mustMatch.add("message", selection.getExpression());
+        searchBool.getMust().add(mustMatch);
+        EsQuery esQuery = new EsQuery(new SearchQuery(searchBool));
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(esQuery);
+    }
+
 
 }
