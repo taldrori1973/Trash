6d5731a433a9a6f686d5498537544e4e767d0520
mohamadi <mohamadi@radware.com>
2020-07-06 15:39:40 +0300

    Jfrog API

---
+++ Auto-Utils/src/main/java/com/radware/vision/automation/AutoUtils/SUT/repositories/pojos/servers/ExternalServers.java
@@ -0,0 +1,9 @@
+package com.radware.vision.automation.AutoUtils.SUT.repositories.pojos.servers;
+
+/**
+ * Created by MohamadI - Muhamad Igbaria
+ * Date: 7/6/2020
+ * Time: 3:39 PM
+ */
+public class ExternalServers {
+}
