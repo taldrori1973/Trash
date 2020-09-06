014929d1da6fe82cbccedc20c2171727eac33c28
ramezh <Ramezh@radware.com>
2020-09-06 15:51:39 +0300

    validateStackBarData offset bug fixed

--- Vision-Auto-Tests/Webui-Step-Handlers/src/main/java/com/radware/vision/infra/testhandlers/vrm/VRMHandler.java
+++ Vision-Auto-Tests/Webui-Step-Handlers/src/main/java/com/radware/vision/infra/testhandlers/vrm/VRMHandler.java
@@ -149,7 +149,7 @@ public class VRMHandler {
                 if (entry.offset != null && entry.offset != 0) {
                     int maxValue = Integer.parseInt(entry.value) + entry.offset;
                     int minValue = Integer.parseInt(entry.value) - entry.offset;
-                    if (!((Integer.parseInt(actualData) > maxValue) || (Integer.parseInt(actualData) < minValue))) {
+                    if ((Integer.parseInt(actualData) > maxValue) || (Integer.parseInt(actualData) < minValue)) {
                         addErrorMessage("The EXPECTED between " + maxValue + " and " + minValue + ", The ACTUAL value of " + entry.legendName + " is " + actualData);
                         scrollAndTakeScreenshot(chart);
                     }
