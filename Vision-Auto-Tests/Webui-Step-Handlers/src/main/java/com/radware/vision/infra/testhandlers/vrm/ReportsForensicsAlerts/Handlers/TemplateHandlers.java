bdbc443408086330c7eedee92f04b30600b77d02
RamezH <ramezh@radware.com>
2020-11-08 16:50:59 +0200

    select widgets, D&D, select options

--- Vision-Auto-Tests/Webui-Step-Handlers/src/main/java/com/radware/vision/infra/testhandlers/vrm/ReportsForensicsAlerts/Handlers/TemplateHandlers.java
+++ Vision-Auto-Tests/Webui-Step-Handlers/src/main/java/com/radware/vision/infra/testhandlers/vrm/ReportsForensicsAlerts/Handlers/TemplateHandlers.java
@@ -1,5 +1,11 @@
 package com.radware.vision.infra.testhandlers.vrm.ReportsForensicsAlerts.Handlers;
 
+import com.radware.automation.tools.basetest.BaseTestUtils;
+import com.radware.automation.tools.basetest.Reporter;
+import com.radware.automation.webui.VisionDebugIdsManager;
+import com.radware.automation.webui.WebUIUtils;
+import com.radware.automation.webui.widgets.ComponentLocator;
+import com.radware.automation.webui.widgets.ComponentLocatorFactory;
 import com.radware.automation.webui.widgets.impl.WebUITextField;
 import com.radware.vision.automation.tools.exceptions.selenium.TargetWebElementNotFoundException;
 import com.radware.vision.automation.tools.sutsystemobjects.devicesinfo.enums.SUTDeviceType;
@@ -7,19 +13,24 @@ import com.radware.vision.infra.testhandlers.baseoperations.BasicOperationsHandl
 import com.radware.vision.infra.testhandlers.vrm.ReportsForensicsAlerts.WebUiTools;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.How;
 
 
-import java.util.ArrayList;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static com.radware.vision.infra.testhandlers.BaseHandler.devicesManager;
+import static org.apache.commons.lang.math.NumberUtils.isNumber;
 
 
 public class TemplateHandlers {
 
     public static void addTemplate(JSONObject templateJsonObject) throws Exception {
         addTemplateType(templateJsonObject.get("reportType").toString());
-        addWidgets(new JSONArray(templateJsonObject.get("Widgets").toString()));
+        addWidgets(new JSONArray(templateJsonObject.get("Widgets").toString()), getCurrentTemplateName(templateJsonObject.get("reportType").toString()));
+
         getScopeSelection(templateJsonObject).create();
     }
 
@@ -40,9 +51,178 @@ public class TemplateHandlers {
         }
     }
 
-    private static void addWidgets(JSONArray widgets) {
+    private static void addWidgets(JSONArray widgets, String reportType) {
+        List<String> widgetsList = getWidgetsList(widgets);
+        List<String> widgetsToRemoveDataDataDebugId = new ArrayList<>();
+        if (widgetsList.contains("ALL")) {
+            widgetsList.removeAll(Collections.singleton("ALL"));
+            widgetsToRemoveDataDataDebugId = null;
+
+        } else {
+            VisionDebugIdsManager.setLabel("selected widget");
+            VisionDebugIdsManager.setParams(reportType);
+            List<WebElement> elements = WebUIUtils.fluentWaitMultiple(new ComponentLocator(How.XPATH, "//*[starts-with(@data-debug-id, '" + VisionDebugIdsManager.getDataDebugId() + "') and contains(@data-debug-id, '_RemoveButton')]").getBy(), WebUIUtils.DEFAULT_WAIT_TIME, false);
+
+            for (WebElement element : elements) {
+                String widgetDataDebugId = element.getAttribute("data-debug-id");
+                String debugId = VisionDebugIdsManager.getDataDebugId();
+                String widgetName = widgetDataDebugId.split(debugId)[1].split("RemoveButton")[0];
+                widgetName = widgetName.substring(1, widgetName.length() - 1);
+                if (!widgetsList.contains(widgetName)) {
+                    widgetsToRemoveDataDataDebugId.add(widgetDataDebugId);
+                } else {
+                    widgetsList.remove(widgetName);
+                }
+            }
+        }
+
+        removeunWantedWidgets(widgetsToRemoveDataDataDebugId);
+        dragAndDropDesiredWidgets(widgetsList, reportType);
+        selectOptions(widgets, getOccurrenceMap(widgets), reportType);
+    }
+
+    private static void removeunWantedWidgets(List<String> widgetsToRemoveDataDataDebugId) {
+        for (String widget : widgetsToRemoveDataDataDebugId) {
+            WebElement widgetElement = WebUIUtils.fluentWait(ComponentLocatorFactory.getLocatorByXpathDbgId(widget).getBy());
+            widgetElement.click();
+        }
+    }
+
+
+    private static void dragAndDropDesiredWidgets(List<String> widgetsList, String reportType) {
+
+        for (String widgetToDrag : widgetsList) {
+            VisionDebugIdsManager.setLabel("widget drag");
+            VisionDebugIdsManager.setParams(widgetToDrag.replaceAll("\\s+", "_"));
+            ComponentLocator sourceLocator = new ComponentLocator(How.XPATH, "//*[@data-debug-id='" + VisionDebugIdsManager.getDataDebugId() + "']");
+            VisionDebugIdsManager.setLabel("Template Header");
+            VisionDebugIdsManager.setParams(reportType);
+//            ComponentLocator targetLocator = new ComponentLocator(How.XPATH, "//*[@data-debug-id='" + VisionDebugIdsManager.getDataDebugId() + "']/..");
+            ComponentLocator targetLocator = new ComponentLocator(How.XPATH, "//*[@class='ReportTemplatestyle__TemplateWidgetsContainer-sc-69xssr-5 iIXqdb']");
+            dragAndDrop(sourceLocator, targetLocator);
+        }
+
 
     }
+
+    private static Map<String, List<Integer>> getOccurrenceMap(JSONArray widgets) {
+
+        Map<String, List<Integer>> ocuurenceMap = new HashMap<>();
+        for (int i = 0; i < widgets.length(); i++) {
+            String widgetName;
+            List<Integer> list = new ArrayList<>();
+            if (widgets.toList().get(i).getClass().getName().contains("HashMap")) {
+                Map<String, Object> widgetMap = new HashMap<String, Object>(((HashMap) widgets.toList().get(i)));
+                List<String> aList = widgetMap.keySet().stream().collect(Collectors.toList());
+                widgetName = aList.get(0);
+//                list.add(it.);
+            } else {
+                widgetName = widgets.getString(i).trim();
+                list.add(null);
+            }
+            if (!ocuurenceMap.containsKey(widgetName)) {
+                List<Integer> indexsList = new ArrayList<>();
+                indexsList.add(i);
+                ocuurenceMap.put(widgetName, indexsList);
+            } else {
+                ocuurenceMap.get(widgetName).add(i);
+            }
+
+        }
+        return ocuurenceMap;
+
+    }
+
+    private static void selectOptions(JSONArray widgets, Map<String, List<Integer>> ocuurenceMap, String reportType) {
+        for (String widgetName : ocuurenceMap.keySet()) {
+            for (int i = 0; i < ocuurenceMap.get(widgetName).size(); i++) {
+                if (widgets.toList().get(ocuurenceMap.get(widgetName).get(i)).getClass().getName().contains("HashMap")) {
+                    Map<String, Object> widgetMap = new HashMap<String, Object>(((HashMap) widgets.toList().get(ocuurenceMap.get(widgetName).get(i))));
+                    List<String> lst = new ArrayList<>((List) widgetMap.get(widgetName));
+                    for (String option : lst) {
+                        switch (option.toLowerCase()) {
+                            case "pps":
+                                VisionDebugIdsManager.setLabel("widget option pps");
+                                VisionDebugIdsManager.setParams(reportType, widgetName);
+                                break;
+                            case "bps":
+                                VisionDebugIdsManager.setLabel("widget option bps");
+                                VisionDebugIdsManager.setParams(reportType, widgetName);
+                                break;
+                            case "ipv4":
+                                VisionDebugIdsManager.setLabel("widget option ipv4");
+                                VisionDebugIdsManager.setParams(reportType, widgetName);
+                                break;
+                            case "ipv6":
+                                VisionDebugIdsManager.setLabel("widget option ipv6");
+                                VisionDebugIdsManager.setParams(reportType, widgetName);
+                                break;
+                            case "inbound":
+                                VisionDebugIdsManager.setLabel("widget option Inbpund");
+                                VisionDebugIdsManager.setParams(reportType, widgetName);
+                                break;
+                            case "outbound":
+                                VisionDebugIdsManager.setLabel("widget option Outbound");
+                                VisionDebugIdsManager.setParams(reportType, widgetName);
+                                break;
+                            case "all policies":
+                                VisionDebugIdsManager.setLabel("widget option All Polices");
+                                VisionDebugIdsManager.setParams(reportType, widgetName);
+                                break;
+                        }
+                        WebUIUtils.fluentWait(ComponentLocatorFactory.getLocatorByXpathDbgId(VisionDebugIdsManager.getDataDebugId()).getBy()).click();
+                        if (isNumber(option)) {
+                            VisionDebugIdsManager.setLabel("widget option CustomPolicies");
+                            VisionDebugIdsManager.setParams(reportType, widgetName);
+//                            int policy= Integer.parseInt(option);
+                            WebUIUtils.fluentWait(ComponentLocatorFactory.getLocatorByXpathDbgId(VisionDebugIdsManager.getDataDebugId()).getBy()).sendKeys(option);
+                        }
+                    }
+                }
+
+            }
+        }
+
+    }
+
+    private static String getCurrentTemplateName(String reportType) {
+        VisionDebugIdsManager.setLabel("Template Header");
+        VisionDebugIdsManager.setParams(reportType);
+        List<WebElement> elements = WebUIUtils.fluentWaitMultiple(new ComponentLocator(How.XPATH, "//*[@data-debug-id='" + VisionDebugIdsManager.getDataDebugId() + "']").getBy(), WebUIUtils.DEFAULT_WAIT_TIME, false);
+        return elements.get(elements.size() - 1).getText();
+
+    }
+
+    public static void dragAndDrop(ComponentLocator sourceLocator, ComponentLocator targetLocator) {
+        try {
+            Actions actions = new Actions(WebUIUtils.getDriver());
+            actions.clickAndHold(WebUIUtils.fluentWait(sourceLocator.getBy()))
+                    .moveByOffset(-10, 0)
+                    .moveToElement(WebUIUtils.fluentWait(targetLocator.getBy()))
+                    .release()
+                    .perform();
+        } catch (Exception e) {
+            BaseTestUtils.report("Failed to Drag and Drop.", Reporter.FAIL);
+            e.printStackTrace();
+        }
+    }
+
+    private static List<String> getWidgetsList(JSONArray widgets) {
+        List<String> widgetsList = new ArrayList<>();
+        for (int i = 0; i < widgets.length(); i++) {
+            if (widgets.toList().get(i).getClass().getName().contains("HashMap")) {
+                Map<String, Object> widgetMap = new HashMap<String, Object>(((HashMap) widgets.toList().get(i)));
+                List<String> aList = widgetMap.keySet().stream().collect(Collectors.toList());
+                widgetsList.addAll(aList);
+            } else {
+
+                widgetsList.add(widgets.getString(i).trim());
+
+            }
+        }
+        return widgetsList;
+    }
+
 
     private static void addTemplateType(String reportType) throws TargetWebElementNotFoundException {
         BasicOperationsHandler.clickButton("Add Template", reportType);
