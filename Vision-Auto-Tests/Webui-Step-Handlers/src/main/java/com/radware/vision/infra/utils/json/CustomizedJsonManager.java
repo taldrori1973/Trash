34332baf69b685868ed5dc64bedf85f88bf5b523
AyoubA <ayouba@radware.com>
2020-11-16 16:29:46 +0200

    fix convert feature to json array

--- Vision-Auto-Tests/Webui-Step-Handlers/src/main/java/com/radware/vision/infra/utils/json/CustomizedJsonManager.java
+++ Vision-Auto-Tests/Webui-Step-Handlers/src/main/java/com/radware/vision/infra/utils/json/CustomizedJsonManager.java
@@ -113,7 +113,7 @@ public class CustomizedJsonManager {
             }
             result.add(res);
         }
-        return (result.toString().split("\\[").length == result.toString().split("]").length + 1?
+        return (new StringBuilder(result.toString()).chars().filter(ch->ch=='[').count() == new StringBuilder(result.toString()).chars().filter(ch->ch==']').count()+1?
                 result.toString().substring(0, result.toString().length()-1): result.toString());
 
     }
