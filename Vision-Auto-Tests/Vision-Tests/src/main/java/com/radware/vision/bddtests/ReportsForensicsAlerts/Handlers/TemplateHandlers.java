e5a4e2ce010be49bad5d18cf8afbacbb5250b862
MahaG <mahag@radware.com>
2020-11-26 14:17:28 +0200

    edit validate summaryTable

--- Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/bddtests/ReportsForensicsAlerts/Handlers/TemplateHandlers.java
+++ Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/bddtests/ReportsForensicsAlerts/Handlers/TemplateHandlers.java
@@ -619,6 +619,8 @@ public class TemplateHandlers {
             case "AppWall":
                 validateTemplateContainsSummryTable(singleActualTemplate, expectedSingleTemplate, errorMessage);
             default:
+                if(expectedSingleTemplate.toMap().containsKey("showTable"))
+                    errorMessage.append("This template  = "+ expectedTemplateTitle+ " do not have summaryTable so do not showTable on expected tamplate!");
                 break;
         }
     }
