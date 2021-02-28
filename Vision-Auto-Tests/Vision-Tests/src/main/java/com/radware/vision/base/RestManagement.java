45c42d31b7cc2d2fbd82257219853f467ca85128
RamezH <ramezh@radware.com>
2021-02-28 10:55:21 +0200

    rest management

--- Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/base/RestManagement.java
+++ Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/base/RestManagement.java
@@ -1,64 +1,21 @@
 package com.radware.vision.base;
 
 import basejunit.RestTestBase;
-import com.radware.automation.bdd.reporter.BddReporterManager;
-import com.radware.automation.tools.basetest.BaseTestUtils;
-import com.radware.automation.tools.licensekeys.LicenseRepository;
-import com.radware.automation.tools.reports.RallyTestReporter;
 import com.radware.restcore.GenericRestClient;
 import com.radware.restcore.RestBasicConsts;
 import com.radware.restcore.VisionRestClient;
 import com.radware.restcore.utils.enums.ExpectedHttpCodes;
-import com.radware.vision.vision_tests.CliTests;
-import jsystem.framework.report.Reporter;
-import systemobjects.VisionLab;
+
 
 public class RestManagement extends RestTestBase {
     public void init() throws Exception {
-//        BaseTestUtils.knownBug(ScenarioHelpers.getRunnerTest(this));
-//        try {
-//            BddReporterManager.overrideJSystemSut();
-//            cliTests = new CliTests();
-//            cliTests.readSutObjects();
-//        } catch (Exception e) {
-//            BaseTestUtils.report("There was issue with reading visionCli: " + e.getMessage(), Reporter.FAIL);
-//        }
-
-//        try {
-//            visionlab = (VisionLab) system.getSystemObject("visionLab");
-//        } catch (Exception e) {
-//            BaseTestUtils.report("There was issue with reading visionLab " + e.getMessage(), Reporter.FAIL);
-//        }
-//        if (visionServer == null)
-//            visionServer = visionlab.visionServer;
-//
-//        if (visionVMs == null)
-//            visionVMs = visionlab.visionVMs;
-//
-//        genericLinuxServer = visionlab.genericLinuxServer;
-//        mysqlServer = visionlab.mysqlClientCli;
         String hostIp = TestBase.getSutManager().getClientConfigurations().getHostIp();
-        String licenseKey = LicenseRepository.getInstance().getLicenseByProject(visionlab.getTestProject());
-//            if (visionServer != null) {
-        visionRestClient = new VisionRestClient(hostIp, licenseKey, RestBasicConsts.RestProtocol.HTTPS);
+        visionRestClient = new VisionRestClient(hostIp, null, RestBasicConsts.RestProtocol.HTTPS);
         visionRestClient.setExpectedStatusCode(ExpectedHttpCodes.OK.getStatusCodes());
         visionRestClient.setIgnoreResponseCodeValidation(ignoreRestResponseValidation);
 
-        genericRestClient = new GenericRestClient(visionServer.getHost(), licenseKey, visionServer.getProtocolEnum());
+        genericRestClient = new GenericRestClient(hostIp, null, RestBasicConsts.RestProtocol.HTTPS);
         genericRestClient.setExpectedStatusCode(ExpectedHttpCodes.OK.getStatusCodes());
         genericRestClient.setIgnoreResponseCodeValidation(ignoreRestResponseValidation);
-
-//            apmRestClient = new ApmRestClient(visionServer.getHost(), licenseKey, visionServer.getProtocolEnum());
-//            BaseTestUtils.reporter.addProperty("Build", getRootServerCli().getBuildNumber());
-//            BaseTestUtils.reporter.addProperty("Version", getRootServerCli().getVersionNumebr());
-//            BaseTestUtils.reporter.addProperty("qcTestId", qcTestId);
-//            initReporter();
-//            if (rallyTestReporter == null) {
-//                rallyTestReporter = new RallyTestReporter();
-//                rallyTestReporter.init(getRootServerCli().getVersionNumebr(), "ReST", getRootServerCli().getBuildNumber());
-//            }
-//            }
-
-
     }
 }
--- Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/base/WebUITestBase.java
+++ Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/base/WebUITestBase.java
@@ -57,7 +57,7 @@ import java.util.*;
 public abstract class WebUITestBase extends TestBase {
     protected boolean doTheVisionLabRestart = false;
     public static String retrievedParamValue = "";
-    public static RestTestBase restTestBase;
+    public static RestManagement restTestBase;
     public static VisionWebUIUtils webUtils;
     public static String restOperationsUsername;
     public static String restOperationsPassword;
@@ -170,14 +170,14 @@ public abstract class WebUITestBase extends TestBase {
         setDeviceName(deviceName);
     }
 
-    public void coreInit() {
+    public void coreInit() throws Exception {
         if (!isRestInit) {
 
             isRestInit = true;
 //
 //            devicesManager = DevicesManager.getInstance("devices");
-//            restTestBase = new RestTestBase();
-//            restTestBase.init();
+            restTestBase = new RestManagement();
+            restTestBase.init();
 //            BaseHandler.restTestBase = restTestBase;
 //            BaseHandler.devicesManager = devicesManager;
         }
