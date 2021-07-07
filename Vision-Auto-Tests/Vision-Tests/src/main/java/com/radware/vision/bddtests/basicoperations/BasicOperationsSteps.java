791746539fe73cb71e015ef41d00b82b9a75d75c
MahaG <mahag@radware.com>
2021-07-07 17:23:52 +0300

    edit scope policy infra

--- Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/bddtests/basicoperations/BasicOperationsSteps.java
+++ Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/bddtests/basicoperations/BasicOperationsSteps.java
@@ -783,7 +783,7 @@ public class BasicOperationsSteps extends BddUITestBase {
 
     private void uiSelectScopePoliciesInDevice(Map<String, String> map) throws Exception {
         Forensics.fixSelectionToArray("devices", map);
-        new TemplateHandlers.DPScopeSelection(new JSONArray(map.get("devices")), "", new JSONObject(map.get("devices")).get("type").toString()).create();
+        new TemplateHandlers.DPScopeSelection(new JSONArray(map.get("devices")), "", new JSONObject(new JSONArray(map.get("devices")).get(0).toString()).get("type").toString()).create();
     }
 
     private void uiUnSelectScopePoliciesInDevice(Map<String, String> map) throws Exception {
