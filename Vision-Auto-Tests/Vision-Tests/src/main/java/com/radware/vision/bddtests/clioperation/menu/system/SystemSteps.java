8a2452d48f3a52c8f7964c5b43957fcc153c2cf4
RotemW <rotemw@radware.com>
2021-08-02 10:15:44 +0300

    Fix TC106038

--- Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/bddtests/clioperation/menu/system/SystemSteps.java
+++ Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/bddtests/clioperation/menu/system/SystemSteps.java
@@ -2,9 +2,10 @@ package com.radware.vision.bddtests.clioperation.menu.system;
 
 import com.radware.automation.tools.basetest.BaseTestUtils;
 import com.radware.automation.tools.basetest.Reporter;
+import com.radware.vision.automation.VisionAutoInfra.CLIInfra.CliOperations;
+import com.radware.vision.automation.VisionAutoInfra.CLIInfra.Servers.RadwareServerCli;
 import com.radware.vision.automation.base.TestBase;
-import com.radware.vision.vision_handlers.common.InvokeCommon;
-import com.radware.vision.vision_handlers.system.SystemGenerals;
+import com.radware.vision.system.SystemGenerals;
 import com.radware.vision.vision_project_cli.menu.Menu;
 import cucumber.api.java.en.Then;
 
@@ -12,13 +13,16 @@ import cucumber.api.java.en.Then;
 
 public class SystemSteps extends TestBase {
 
+    private final RadwareServerCli radwareServerCli = serversManagement.getRadwareServerCli().get();
+
+
     @Then("^CLI Verifying sub menu of system cmd$")
-    public void system(){
-try {
-    InvokeCommon.checkSubMenu(restTestBase.getRadwareServerCli(), Menu.system().build(), SystemGenerals.SYSTEM_SUB_MENU);
-}catch(Exception e){
-    BaseTestUtils.report(e.getMessage(), Reporter.FAIL);
-}
+    public void system() {
+        try {
+            CliOperations.checkSubMenu(radwareServerCli, Menu.system().build(), SystemGenerals.SYSTEM_SUB_MENU);
+        } catch (Exception e) {
+            BaseTestUtils.report(e.getMessage(), Reporter.FAIL);
+        }
 
     }
 }
