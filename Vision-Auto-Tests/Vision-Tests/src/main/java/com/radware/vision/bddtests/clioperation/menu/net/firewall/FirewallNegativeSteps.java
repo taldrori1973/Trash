2b581358a906164d96c3c547d949c8f4c3ff7418
RotemW <rotemw@radware.com>
2021-08-05 12:52:10 +0300

    Add new test case TC122604

--- Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/bddtests/clioperation/menu/net/firewall/FirewallNegativeSteps.java
+++ Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/bddtests/clioperation/menu/net/firewall/FirewallNegativeSteps.java
@@ -1,22 +1,28 @@
 package com.radware.vision.bddtests.clioperation.menu.net.firewall;
 
+import com.radware.automation.tools.basetest.BaseTestUtils;
+import com.radware.automation.tools.basetest.Reporter;
 import com.radware.vision.automation.VisionAutoInfra.CLIInfra.menu.Menu;
 import com.radware.vision.automation.base.TestBase;
-import com.radware.vision.vision_tests.CliNegativeTests;
+import com.radware.vision.bddtests.CliNegative;
 import cucumber.api.java.en.Given;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 
 public class FirewallNegativeSteps extends TestBase {
 
-    CliNegativeTests cliNegativeTests=new CliNegativeTests();
+    CliNegative cliNegative = new CliNegative();
 
-    @Given("^CLI Net Firewall open-port set negative$")
-    public void openportSet() throws Exception{
-        cliNegativeTests.init();
-        ArrayList<CliNegativeTests.InvalidInputDataType> invailedDataList = new ArrayList<CliNegativeTests.InvalidInputDataType>(Arrays.asList(CliNegativeTests.InvalidInputDataType.FIREWALL_OPENPORT_SET_NEGATIVE));
-        cliNegativeTests.run(Menu.net().firewall().openport().set().build(), invailedDataList , CliNegativeTests.GoodErrorsList.NET_FIREWALL_LIST);
-        cliNegativeTests.after();
+    @Given("^CLI Net Firewall open-port set Negative$")
+    public void openPortSet(){
+        try {
+            cliNegative.init();
+            ArrayList<CliNegative.InvalidInputDataType> invalidDataList = new ArrayList<>(Collections.singletonList(CliNegative.InvalidInputDataType.FIREWALL_OPENPORT_SET_NEGATIVE));
+            cliNegative.run(Menu.net().firewall().openport().set().build(), invalidDataList , " open", CliNegative.GoodErrorsList.NET_FIREWALL_LIST);
+            cliNegative.after();
+        } catch (Exception e) {
+            BaseTestUtils.report(e.getMessage(), Reporter.FAIL);
+        }
     }
 }
