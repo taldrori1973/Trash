2c4ea4d1751c0d7b3ac5fba2e254c194b93fb5f8
SapirG <sapirg@radware.com>
2021-08-16 11:28:31 +0300

    fix bug and added dependency with local systemPath (temporary)

--- Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/thirdPartyAPIs/SaproCommunication/SaproCommunicationHandler.java
+++ Vision-Auto-Tests/Vision-Tests/src/main/java/com/radware/vision/thirdPartyAPIs/SaproCommunication/SaproCommunicationHandler.java
@@ -9,9 +9,11 @@ import com.sapcnsl.exception.SaproException;
 import com.sapcnsl.api.returntypes.MapListInfo;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class SaproCommunicationHandler {
     private final String mapDirectoryName;
+    private final String xmlFullPath;
     private final SaproCommunication saproObj;
     private final String inetAddress;
     private final int port;
@@ -19,9 +21,11 @@ public class SaproCommunicationHandler {
     public SaproCommunicationHandler() {
         SaproCommunicationFactory factoryObj = SaproCommunicationFactory.sessionFactory;
         this.saproObj = factoryObj.getNewSession();
+        this.saproObj.setNewVersion(true);
         this.inetAddress = "172.17.166.8";
         this.port = 2100;
         mapDirectoryName = "/opt/sapro/map/";
+        xmlFullPath = "/opt/sapro/xml/";
     }
 
 
@@ -60,17 +64,17 @@ public class SaproCommunicationHandler {
      *                 if dev is already on - nothing will change
      *                 if dev is off - start dev and also start (if necessary) the map which dev belongs to
      */
-    public void startDevicesFromMap(String mapName, List<String> devNames) {
+    public void startDevicesFromMap(String mapName, String... devNames) {
         try {
             this.initConnect();
             mapName = this.getFullMapName(mapName);
-            List<MapListInfo> l = saproObj.getMapListFromServer();
+//            List<MapListInfo> l = saproObj.getMapListFromServer();
             for (String devName : devNames) {
                 BaseTestUtils.report("Starting device " + devName + " from " + mapName, Reporter.PASS_NOR_FAIL);
                 CommandMessage start = saproObj.sendStartCmdToDevice(mapName, devName);
                 BaseTestUtils.report(start.getMessage(), Reporter.PASS);
-                this.closeConnect();
             }
+            this.closeConnect();
         } catch (SaproException e) {
             BaseTestUtils.report(e.getMessage(), Reporter.FAIL);
         }
@@ -78,18 +82,18 @@ public class SaproCommunicationHandler {
 
     /**
      * @param mapName  - the name of the map (not full name)
-     * @param devNames - list of all devices that need to be started
-     *                 if dev is already off - nothing will change, if dev is on - stop dev from given map
+     * @param devNames - array of all devices that need to be started
      */
-    public void stopDevicesFromMap(String mapName, List<String> devNames) {
+//    public void stopDevicesFromMap(String mapName, List<String> devNames) {
+    public void stopDevicesFromMap(String mapName, String... devNames) {
         try {
             this.initConnect();
             mapName = this.getFullMapName(mapName);
             for (String devName : devNames) {
                 CommandMessage stop = saproObj.sendStopCmdToDevice(mapName, devName);
                 BaseTestUtils.report(stop.getMessage(), Reporter.PASS);
-                this.closeConnect();
             }
+            this.closeConnect();
         } catch (SaproException e) {
             BaseTestUtils.report(e.getMessage(), Reporter.FAIL);
         }
@@ -144,6 +148,31 @@ public class SaproCommunicationHandler {
     }
 
     /**
+     * @param mapName    - the name of the map (not full name)
+     * @param deviceName - the name of the device
+     * @param newXmlFile - the full path of new xml modeling file we want to reload
+     *                   Using tcl command 'SA_reload_xmlmodfile' to reload the new xml file
+     *                   The new file is playing only during runtime
+     */
+    public void reloadXmlFile(String mapName, String deviceName, String newXmlFile) {
+        try {
+            // making sure the device is on:
+            this.startDevicesFromMap(mapName, deviceName);
+            this.initConnect();
+            CommandMessage reload = saproObj.sendTclCmdToDevice(this.getFullMapName(mapName), deviceName, "SA_reload_xmlmodfile " + this.xmlFullPath + newXmlFile);
+            BaseTestUtils.report(reload.getMessage(), Reporter.PASS);
+            this.closeConnect();
+        } catch (SaproException e) {
+            BaseTestUtils.report(e.getMessage(), Reporter.FAIL);
+        }
+    }
+
+    public void refresh() throws SaproException {
+        saproObj.getStatsFromMap("/opt/sapro/map/Danny.map");
+    }
+
+
+    /**
      * @param mapName gets all files from mapDirectoryName and use mapName as filter
      *                NOTE - assuming there is already connection with the server
      * @return full map name if there is match between mapName to one of the maps
@@ -157,15 +186,6 @@ public class SaproCommunicationHandler {
                     return mapDirectoryName + m;
                 }
             }
-//            List<MapListInfo> mapList = saproObj.getMapDevListFromServer();
-//            for (MapListInfo map : mapList) {
-//                String[] parts = map.getMapName().split("/");
-//                String lastPart = parts[parts.length - 1];
-//                String name = lastPart.substring(0, lastPart.indexOf('.'));
-//                if (name.equals(mapName)) {
-//                    return map.getMapName();
-//                }
-//            }
         } catch (SaproException e) {
             BaseTestUtils.report(e.getMessage(), Reporter.FAIL);
         }
