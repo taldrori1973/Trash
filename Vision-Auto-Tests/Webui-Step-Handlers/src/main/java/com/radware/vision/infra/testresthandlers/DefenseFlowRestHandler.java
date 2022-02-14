8f40202b6dcb383153c5d248a4ca75fe4dd7a2f4
mahmo <mahmouds@radware.com>
2022-02-14 10:49:49 +0200

    Fix DefenseFlowRestHandler - use restTestBase from extends TestBase

--- Vision-Auto-Tests/Webui-Step-Handlers/src/main/java/com/radware/vision/infra/testresthandlers/DefenseFlowRestHandler.java
+++ Vision-Auto-Tests/Webui-Step-Handlers/src/main/java/com/radware/vision/infra/testresthandlers/DefenseFlowRestHandler.java
@@ -1,23 +1,23 @@
 package com.radware.vision.infra.testresthandlers;
 
 import com.radware.restcore.utils.enums.HttpMethodEnum;
-import com.radware.vision.infra.testhandlers.BaseHandler;
+import com.radware.vision.automation.base.TestBase;
 import testhandlers.VisionRestApiHandler;
 
-public class DefenseFlowRestHandler {
+public class DefenseFlowRestHandler extends TestBase {
 
     public static String addOrGetPo(HttpMethodEnum method, String urlField, String bodyField, String expectedResult) {
         String result;
 
         VisionRestApiHandler visionRestApiHandler = new VisionRestApiHandler();
-        result = (String) visionRestApiHandler.handleRequest(BaseHandler.restTestBase.getVisionRestClient(), method,
+        result = (String) visionRestApiHandler.handleRequest(restTestBase.getVisionRestClient(), method,
                 "DefenseFlow->Add New PO", urlField, bodyField, expectedResult);
         return result;
     }
 
     public static void deletePo(HttpMethodEnum method,String urlField, String bodyField, String expectedResult){
         VisionRestApiHandler visionRestApiHandler = new VisionRestApiHandler();
-        visionRestApiHandler.handleRequest(BaseHandler.restTestBase.getVisionRestClient(), method,
+        visionRestApiHandler.handleRequest(restTestBase.getVisionRestClient(), method,
                 "DefenseFlow->Delete PO", urlField, bodyField, expectedResult);
     }
 
