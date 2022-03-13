d0e19ce3b9381242031ad28121f4595124e4f6da
RotemW <rotemw@radware.com>
2022-03-13 14:29:08 +0200

    Add new Http Reporting CLI support

--- Vision-Auto-Infra/CLI-Infra/src/main/java/com/radware/vision/automation/VisionAutoInfra/CLIInfra/menu/system/http_reporting/mode/Mode.java
+++ Vision-Auto-Infra/CLI-Infra/src/main/java/com/radware/vision/automation/VisionAutoInfra/CLIInfra/menu/system/http_reporting/mode/Mode.java
@@ -1,14 +1,13 @@
 package com.radware.vision.automation.VisionAutoInfra.CLIInfra.menu.system.http_reporting.mode;
 
 import com.radware.vision.automation.VisionAutoInfra.CLIInfra.menu.Builder;
-import com.radware.vision.automation.VisionAutoInfra.CLIInfra.menu.common.Get;
 
 public class Mode extends Builder {
     public Mode(String prefix) {
         super(prefix);
     }
 
-    public com.radware.vision.automation.VisionAutoInfra.CLIInfra.menu.common.Get get() {return new Get(build());}
+    public Get get() {return new Get(build());}
 
     public SetOn setOn() {return new SetOn(build());}
 
